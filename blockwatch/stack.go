a17575fe39b03153f42a3b59c595b030e12bc9a5
Fabio Berger <me@fabioberger.com>
2019-03-29 22:58:20 +0000

    Make Stack go-routine safe

--- blockwatch/stack.go
+++ blockwatch/stack.go
@@ -1,10 +1,14 @@
 package blockwatch
 
-import "container/list"
+import (
+	"container/list"
+	"sync"
+)
 
 // Stack allows performing basic stack operations on a stack of MiniBlockHeaders.
 type Stack struct {
 	list *list.List
+	mut  sync.Mutex
 }
 
 func NewStack() *Stack {
@@ -15,6 +19,8 @@ func NewStack() *Stack {
 
 // Pop removes and returns the latest block on the block stack.
 func (bs *Stack) Pop() *MiniBlockHeader {
+	bs.mut.Lock()
+	defer bs.mut.Unlock()
 	block := bs.list.Front()
 	bs.list.Remove(block)
 	return block.Value.(*MiniBlockHeader)
@@ -22,11 +28,15 @@ func (bs *Stack) Pop() *MiniBlockHeader {
 
 // Push pushes a block onto the block stack.
 func (bs *Stack) Push(block *MiniBlockHeader) {
+	bs.mut.Lock()
+	defer bs.mut.Unlock()
 	bs.list.PushFront(block)
 }
 
 // Peek returns the latest block from the block stack without removing it.
 func (bs *Stack) Peek() *MiniBlockHeader {
+	bs.mut.Lock()
+	defer bs.mut.Unlock()
 	block := bs.list.Front()
 	if block == nil {
 		return nil
@@ -37,6 +47,8 @@ func (bs *Stack) Peek() *MiniBlockHeader {
 // Inspect returns all the blocks in the stack. This method should only be used for debugging
 // and testing purposes. It is not performant.
 func (bs *Stack) Inspect() []*MiniBlockHeader {
+	bs.mut.Lock()
+	defer bs.mut.Unlock()
 	blocks := []*MiniBlockHeader{}
 	for e := bs.list.Back(); e != nil; e = e.Prev() {
 		blocks = append(blocks, e.Value.(*MiniBlockHeader))
