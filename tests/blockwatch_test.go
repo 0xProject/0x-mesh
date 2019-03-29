f486102ab9b07f630a9d819070f5da51f620b44b
Fabio Berger <me@fabioberger.com>
2019-03-29 21:45:34 +0000

    Use testify in tests

--- tests/blockwatch_test.go
+++ tests/blockwatch_test.go
@@ -2,13 +2,11 @@ package tests
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"reflect"
 	"testing"
 
 	"github.com/0xProject/0x-mesh/blockwatch"
 	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/stretchr/testify/assert"
 )
 
 // TestBlockWatch tests that BlockWatch properly stores up to blockRetentionLimit blocks in the correct
@@ -26,37 +24,13 @@ func TestBlockWatch(t *testing.T) {
 		bs.PollNextBlock(context.Background())
 		retainedBlocks := bs.GetRetainedBlocks()
 		expectedRetainedBlocks := fakeBlockClient.ExpectedRetainedBlocks()
-		if !reflect.DeepEqual(retainedBlocks, expectedRetainedBlocks) {
-			retainedBlocksJson, err := json.MarshalIndent(retainedBlocks, "", "	")
-			if err != nil {
-				panic(err)
-			}
-			expectedRetainedBlocksJson, err := json.MarshalIndent(expectedRetainedBlocks, "", "	")
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("GOT BLOCKS: %v\n", string(retainedBlocksJson))
-			fmt.Printf("EXPECTED BLOCK: %v\n", string(expectedRetainedBlocksJson))
-			t.Fatal("Failed retained block test: ", scenarioLabel)
-		}
+		assert.Equal(t, expectedRetainedBlocks, retainedBlocks, scenarioLabel)
 
 		expectedBlockEvents := fakeBlockClient.GetBlockEvents()
 		// If we expect events to be emitted, check them
 		if len(expectedBlockEvents) != 0 {
 			gotBlockEvents := <-bs.Events
-			if !reflect.DeepEqual(expectedBlockEvents, gotBlockEvents) {
-				gotBlockEventsJson, err := json.MarshalIndent(gotBlockEvents, "", "	")
-				if err != nil {
-					panic(err)
-				}
-				expectedBlockEventsJson, err := json.MarshalIndent(expectedBlockEvents, "", "	")
-				if err != nil {
-					panic(err)
-				}
-				fmt.Printf("GOT EVENTS: %v\n", string(gotBlockEventsJson))
-				fmt.Printf("EXPECTED EVENTS: %v\n", string(expectedBlockEventsJson))
-				t.Fatal("Failed emitted event test:", scenarioLabel)
-			}
+			assert.Equal(t, expectedBlockEvents, gotBlockEvents, scenarioLabel)
 		}
 
 		fakeBlockClient.IncrementTimestep()
