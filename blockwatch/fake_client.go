c49dfb1eafc804643f48e988a5f37684a4ae1c1f
Fabio Berger <me@fabioberger.com>
2019-04-02 09:19:12 -0500

    Add mutex around fixtureData accesses

--- blockwatch/fake_client.go
+++ blockwatch/fake_client.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io/ioutil"
 	"math/big"
+	"sync"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
@@ -24,6 +25,7 @@ type fixtureTimestep struct {
 type fakeClient struct {
 	currentTimestep uint
 	fixtureData     []fixtureTimestep
+	fixtureMut      sync.Mutex
 }
 
 // newFakeClient instantiates a fakeClient for testing purposes.
@@ -37,12 +39,14 @@ func newFakeClient() (*fakeClient, error) {
 	_ = json.Unmarshal(blob, &fixtureData)
 
 	var startTimestep uint = 0
-	return &fakeClient{startTimestep, fixtureData}, nil
+	return &fakeClient{currentTimestep: startTimestep, fixtureData: fixtureData}, nil
 }
 
 // HeaderByNumber fetches a block header by its number. If no `number` is supplied, it will return the latest
 // block header. If no block exists with this number it will return a `ethereum.NotFound` error.
 func (fc *fakeClient) HeaderByNumber(number *big.Int) (*MiniHeader, error) {
+	fc.fixtureMut.Lock()
+	defer fc.fixtureMut.Unlock()
 	timestep := fc.fixtureData[fc.currentTimestep]
 	var miniHeader MiniHeader
 	var ok bool
@@ -60,6 +64,8 @@ func (fc *fakeClient) HeaderByNumber(number *big.Int) (*MiniHeader, error) {
 // HeaderByHash fetches a block header by its block hash. If no block exists with this number it will return
 // a `ethereum.NotFound` error.
 func (fc *fakeClient) HeaderByHash(hash common.Hash) (*MiniHeader, error) {
+	fc.fixtureMut.Lock()
+	defer fc.fixtureMut.Unlock()
 	timestep := fc.fixtureData[fc.currentTimestep]
 	miniHeader, ok := timestep.GetBlockByHash[hash]
 	if !ok {
@@ -75,22 +81,30 @@ func (fc *fakeClient) IncrementTimestep() {
 
 // NumberOfTimesteps returns the number of timesteps in the simulation
 func (fc *fakeClient) NumberOfTimesteps() int {
+	fc.fixtureMut.Lock()
+	defer fc.fixtureMut.Unlock()
 	return len(fc.fixtureData)
 }
 
 // ExpectedRetainedBlocks returns the expected retained blocks at the current timestep.
 func (fc *fakeClient) ExpectedRetainedBlocks() []*MiniHeader {
+	fc.fixtureMut.Lock()
+	defer fc.fixtureMut.Unlock()
 	return fc.fixtureData[fc.currentTimestep].GetCorrectChain
 }
 
 // GetScenarioLabel returns a label describing the test case being tested by the current timestep
 // of the simulation.
 func (fc *fakeClient) GetScenarioLabel() string {
+	fc.fixtureMut.Lock()
+	defer fc.fixtureMut.Unlock()
 	return fc.fixtureData[fc.currentTimestep].ScenarioLabel
 }
 
 // GetEvents returns the events in the order they should have been emitted by Watcher for
 // the current timestep of the simulation.
 func (fc *fakeClient) GetEvents() []*Event {
+	fc.fixtureMut.Lock()
+	defer fc.fixtureMut.Unlock()
 	return fc.fixtureData[fc.currentTimestep].BlockEvents
 }
