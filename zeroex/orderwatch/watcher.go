bad682051503cb744c103eb36b166b03722328f7
Fabio Berger <me@fabioberger.com>
2019-04-08 19:12:59 -0500

    Add teardown method

--- zeroex/orderwatch/watcher.go
+++ zeroex/orderwatch/watcher.go
@@ -60,6 +60,22 @@ func (w *Watcher) Setup(expirationPollingInterval time.Duration) error {
 	return nil
 }
 
+func (w *Watcher) Teardown() error {
+	w.setupMux.Lock()
+	if !w.isSetup {
+		w.setupMux.Unlock()
+		return errors.New("Cannot teardown before calling Setup()")
+	}
+	w.setupMux.Unlock()
+
+	// Stop event subscription
+	w.blockSubscription.Unsubscribe()
+
+	// Stop expiration watcher
+	w.expirationWatcher.Stop()
+	return nil
+}
+
 // Watch adds a 0x order to the ones being tracked for order-relevant state changes
 func (w *Watcher) Watch(signedOrder *zeroex.SignedOrder, orderHash common.Hash) error {
 	w.setupMux.Lock()
@@ -102,162 +118,176 @@ func (w *Watcher) setupExpirationWatcher(expirationPollingInterval time.Duration
 
 func (w *Watcher) setupEventWatcher() {
 	blockEvents := make(chan []*blockwatch.Event, 10)
-	sub := w.blockWatcher.Subscribe(blockEvents)
+	w.blockSubscription = w.blockWatcher.Subscribe(blockEvents)
 
 	go func() {
-		defer sub.Unsubscribe()
-		for events := range blockEvents {
-			for _, event := range events {
-				for _, log := range event.BlockHeader.Logs {
-					eventType, err := w.eventDecoder.FindEventType(log)
-					if err != nil {
-						switch err.(type) {
-						case UntrackedTokenError:
-							continue
-						case UnsupportedEventError:
-							// TODO(fabio): Log the event topics
-							continue
+		for {
+			select {
+			case err, isOpen := <-w.blockSubscription.Err():
+				close(blockEvents)
+				if !isOpen {
+					// event.Subscription closes the Error channel on unsubscribe.
+					// We therefore cleanup this goroutine on channel closure.
+					return
+				}
+				// TODO(fabio): Log subscription error
+				fmt.Println(err)
+				return
+
+			case events := <-blockEvents:
+				for _, event := range events {
+					for _, log := range event.BlockHeader.Logs {
+						eventType, err := w.eventDecoder.FindEventType(log)
+						if err != nil {
+							switch err.(type) {
+							case UntrackedTokenError:
+								continue
+							case UnsupportedEventError:
+								// TODO(fabio): Log the event topics
+								continue
+							default:
+								panic(err)
+							}
+						}
+						switch eventType {
+						case "ERC20TransferEvent":
+							var transferEvent ERC20TransferEvent
+							err = w.eventDecoder.Decode(log, &transferEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
+						case "ERC20ApprovalEvent":
+							var approvalEvent ERC20ApprovalEvent
+							err = w.eventDecoder.Decode(log, &approvalEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
+						case "ERC721TransferEvent":
+							var transferEvent ERC721TransferEvent
+							err = w.eventDecoder.Decode(log, &transferEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
+						case "ERC721ApprovalEvent":
+							var approvalEvent ERC721ApprovalEvent
+							err = w.eventDecoder.Decode(log, &approvalEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
+						case "ERC721ApprovalForAllEvent":
+							var approvalForAllEvent ERC721ApprovalForAllEvent
+							err = w.eventDecoder.Decode(log, &approvalForAllEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
+						case "WethWithdrawalEvent":
+							var withdrawalEvent WethWithdrawalEvent
+							err = w.eventDecoder.Decode(log, &withdrawalEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
+						case "WethDepositEvent":
+							var depositEvent WethDepositEvent
+							err = w.eventDecoder.Decode(log, &depositEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
+						case "ExchangeFillEvent":
+							var exchangeFillEvent ExchangeFillEvent
+							err = w.eventDecoder.Decode(log, &exchangeFillEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
+						case "ExchangeCancelEvent":
+							var exchangeCancelEvent ExchangeCancelEvent
+							err = w.eventDecoder.Decode(log, &exchangeCancelEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
+						case "ExchangeCancelUpToEvent":
+							var exchangeCancelUpToEvent ExchangeCancelUpToEvent
+							err = w.eventDecoder.Decode(log, &exchangeCancelUpToEvent)
+							if err != nil {
+								switch err.(type) {
+								case UnsupportedEventError:
+									// TODO(fabio): Log the event topics
+									continue
+								default:
+									panic(err)
+								}
+							}
+							// TODO(fabio): Handle this event
 						default:
-							panic(err)
+							panic(fmt.Sprintf("Did not handle event %s\n", eventType))
 						}
-					}
-					switch eventType {
-					case "ERC20TransferEvent":
-						var transferEvent ERC20TransferEvent
-						err = w.eventDecoder.Decode(log, &transferEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					case "ERC20ApprovalEvent":
-						var approvalEvent ERC20ApprovalEvent
-						err = w.eventDecoder.Decode(log, &approvalEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					case "ERC721TransferEvent":
-						var transferEvent ERC721TransferEvent
-						err = w.eventDecoder.Decode(log, &transferEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					case "ERC721ApprovalEvent":
-						var approvalEvent ERC721ApprovalEvent
-						err = w.eventDecoder.Decode(log, &approvalEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					case "ERC721ApprovalForAllEvent":
-						var approvalForAllEvent ERC721ApprovalForAllEvent
-						err = w.eventDecoder.Decode(log, &approvalForAllEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					case "WethWithdrawalEvent":
-						var withdrawalEvent WethWithdrawalEvent
-						err = w.eventDecoder.Decode(log, &withdrawalEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					case "WethDepositEvent":
-						var depositEvent WethDepositEvent
-						err = w.eventDecoder.Decode(log, &depositEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					case "ExchangeFillEvent":
-						var exchangeFillEvent ExchangeFillEvent
-						err = w.eventDecoder.Decode(log, &exchangeFillEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					case "ExchangeCancelEvent":
-						var exchangeCancelEvent ExchangeCancelEvent
-						err = w.eventDecoder.Decode(log, &exchangeCancelEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					case "ExchangeCancelUpToEvent":
-						var exchangeCancelUpToEvent ExchangeCancelUpToEvent
-						err = w.eventDecoder.Decode(log, &exchangeCancelUpToEvent)
-						if err != nil {
-							switch err.(type) {
-							case UnsupportedEventError:
-								// TODO(fabio): Log the event topics
-								continue
-							default:
-								panic(err)
-							}
-						}
-						// TODO(fabio): Handle this event
-					default:
-						panic(fmt.Sprintf("Did not handle event %s\n", eventType))
 					}
 				}
 			}
 		}
+
 	}()
 }
 
