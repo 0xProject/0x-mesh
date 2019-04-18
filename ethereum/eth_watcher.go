d494abbbea99b9d11559b11c9c8730e8bf5dcb7e
Fabio Berger <me@fabioberger.com>
2019-04-18 18:36:40 -0500

    Implement initial eth_watcher and tests

---
+++ ethereum/eth_watcher.go
@@ -0,0 +1,161 @@
+package ethereum
+
+import (
+	"errors"
+	"math/big"
+	"sync"
+	"time"
+
+	"github.com/0xproject/0x-mesh/ethereum/wrappers"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+// The most addresses we can fetch balances for in a single CALL without having Parity nor Geth
+// timeout
+const chunkSize = 4000
+
+// Balance represents a single Ethereum addresses Ether balance
+type Balance struct {
+	Address common.Address
+	Balance *big.Int
+}
+
+// ETHWatcher allows for watching a set of Ethereum addresses for ETH balance
+// changes
+type ETHWatcher struct {
+	addressToBalance         map[common.Address]*big.Int
+	pollingInterval          time.Duration
+	isWatching               bool
+	balanceChan              chan Balance
+	ethBalanceCheckerAddress common.Address
+	ethClient                *ethclient.Client
+	ticker                   *time.Ticker
+	addressToBalanceMu       sync.Mutex
+}
+
+// NewETHWatcher creates a new instance of ETHWatcher
+func NewETHWatcher(pollingInterval time.Duration, ethClient *ethclient.Client, ethBalanceCheckerAddress common.Address) *ETHWatcher {
+	return &ETHWatcher{
+		addressToBalance:         make(map[common.Address]*big.Int),
+		balanceChan:              make(chan Balance, 100),
+		pollingInterval:          pollingInterval,
+		ethBalanceCheckerAddress: ethBalanceCheckerAddress,
+		isWatching:               false,
+		ethClient:                ethClient,
+	}
+}
+
+// Start begins the ETH balance poller
+func (e *ETHWatcher) Start() error {
+	if e.isWatching {
+		return errors.New("Watcher already started")
+	}
+	e.isWatching = true
+	e.ticker = time.NewTicker(e.pollingInterval)
+	go func() {
+		for {
+			<-e.ticker.C
+
+			// TODO(fabio): Currently if `updateBalance` takes longer then the ticker interval,
+			// we would kick off an additional call before the previous one completed. This might
+			// not be desirable, and we might want to noop if a previous request is in progress.
+			e.updateBalances()
+		}
+	}()
+	return nil
+}
+
+// Stop stops the ETH balance poller
+func (e *ETHWatcher) Stop() {
+	if !e.isWatching {
+		return // noop
+	}
+	e.ticker.Stop()
+	e.isWatching = false
+}
+
+// Add adds a new Ethereum address we'd like to track for balance changes
+func (e *ETHWatcher) Add(address common.Address, initialBalance *big.Int) {
+	e.addressToBalanceMu.Lock()
+	defer e.addressToBalanceMu.Unlock()
+	if _, ok := e.addressToBalance[address]; ok {
+		return // Noop. Already exists and we bias towards our existing balance
+	}
+	e.addressToBalance[address] = initialBalance
+}
+
+// Remove removes a new Ethereum address we no longer want to track for balance changes
+func (e *ETHWatcher) Remove(address common.Address) {
+	e.addressToBalanceMu.Lock()
+	defer e.addressToBalanceMu.Unlock()
+	if _, ok := e.addressToBalance[address]; ok {
+		delete(e.addressToBalance, address)
+	}
+}
+
+// GetBalance returns the Ether balance for a particular Ethereum address
+func (e *ETHWatcher) GetBalance(address common.Address) (*big.Int, error) {
+	e.addressToBalanceMu.Lock()
+	defer e.addressToBalanceMu.Unlock()
+	if balance, ok := e.addressToBalance[address]; ok {
+		return balance, nil
+	}
+	return nil, errors.New("Supplied address not tracked")
+}
+
+// Receive returns a read-only channel that can be used to listen for modified ETH balances
+func (e *ETHWatcher) Receive() <-chan Balance {
+	return e.balanceChan
+}
+
+func (e *ETHWatcher) updateBalances() error {
+	e.addressToBalanceMu.Lock()
+	addresses := []common.Address{}
+	for address := range e.addressToBalance {
+		addresses = append(addresses, address)
+	}
+	e.addressToBalanceMu.Unlock()
+
+	chunks := [][]common.Address{}
+	// Chunk into groups of chunkSize addresses for the call
+	for len(addresses) > chunkSize {
+		chunks = append(chunks, addresses[:chunkSize])
+		addresses = addresses[chunkSize:]
+	}
+	if len(addresses) > 0 {
+		chunks = append(chunks, addresses)
+	}
+
+	ethBalanceChecker, err := wrappers.NewEthBalanceChecker(e.ethBalanceCheckerAddress, e.ethClient)
+	if err != nil {
+		return err
+	}
+	for _, chunk := range chunks {
+		// Call contract for each chunk of addresses in parallel
+		go func() {
+			balances, err := ethBalanceChecker.GetEthBalances(nil, chunk)
+			if err != nil {
+				// TODO(fabio): Log error
+				return // Noop on failure, simply wait for next polling interval
+			}
+			for i, address := range chunk {
+				e.addressToBalanceMu.Lock()
+				if balance, ok := e.addressToBalance[address]; ok {
+					if balance != balances[i] {
+						e.addressToBalance[address] = balances[i]
+						updatedBalance := Balance{
+							Address: address,
+							Balance: balances[i],
+						}
+						go func() {
+							e.balanceChan <- updatedBalance
+						}()
+					}
+				}
+				e.addressToBalanceMu.Unlock()
+			}
+		}()
+	}
+	return nil
+}
--- ethereum/eth_watcher_test.go
+++ ethereum/eth_watcher_test.go
@@ -1,145 +1,108 @@
+// +build !js
+// HACK(fabio): We currently don't run these tests in WASM because the `roundtrip_js.go` file in
+// Go's net/http special-cases requests originating from test files, routing the requests to a fake
+// in-process network handler. This causes the tests to fail.
+// Source: https://github.com/golang/go/issues/31559
+
 package ethereum
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+	"math/big"
 	"testing"
+	"time"
+
+	"github.com/0xproject/0x-mesh/config"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/stretchr/testify/assert"
 )
 
-// var ethAccountToBalance = map[common.Address]*big.Int{
-// 	common.HexToAddress("0x5409ed021d9299bf6814279a6a1411a7e866a631"): firstAccountBalance,
-// 	common.HexToAddress("0x6ecbe1db9ef729cbe972c83fb886247691fb6beb"): hundredEth,
-// 	common.HexToAddress("0xe36ea790bc9d7ab70c55260c66d52b1eca985f84"): hundredEth,
-// 	common.HexToAddress("0xe834ec434daba538cd1b9fe1582052b880bd7e63"): hundredEth,
-// 	common.HexToAddress("0x78dc5d2d739606d31509c31d654056a45185ecb6"): hundredEth,
-// 	common.HexToAddress("0xa8dda8d7f5310e4a9e24f8eba77e091ac264f872"): hundredEth,
-// 	common.HexToAddress("0x06cef8e666768cc40cc78cf93d9611019ddcb628"): hundredEth,
-// 	common.HexToAddress("0x4404ac8bd8f9618d27ad2f1485aa1b2cfd82482d"): hundredEth,
-// 	common.HexToAddress("0x7457d5e02197480db681d3fdf256c7aca21bdc12"): hundredEth,
-// 	common.HexToAddress("0x91c987bf62d25945db517bdaa840a6c661374402"): hundredEth,
-// }
+var ethAccountToBalance = map[common.Address]*big.Int{
+	common.HexToAddress("0x5409ed021d9299bf6814279a6a1411a7e866a631"): firstAccountBalance,
+	common.HexToAddress("0x6ecbe1db9ef729cbe972c83fb886247691fb6beb"): hundredEth,
+	common.HexToAddress("0xe36ea790bc9d7ab70c55260c66d52b1eca985f84"): hundredEth,
+	common.HexToAddress("0xe834ec434daba538cd1b9fe1582052b880bd7e63"): hundredEth,
+	common.HexToAddress("0x78dc5d2d739606d31509c31d654056a45185ecb6"): hundredEth,
+	common.HexToAddress("0xa8dda8d7f5310e4a9e24f8eba77e091ac264f872"): hundredEth,
+	common.HexToAddress("0x06cef8e666768cc40cc78cf93d9611019ddcb628"): hundredEth,
+	common.HexToAddress("0x4404ac8bd8f9618d27ad2f1485aa1b2cfd82482d"): hundredEth,
+	common.HexToAddress("0x7457d5e02197480db681d3fdf256c7aca21bdc12"): hundredEth,
+	common.HexToAddress("0x91c987bf62d25945db517bdaa840a6c661374402"): hundredEth,
+}
 
-// var firstAccountBalance, _ = math.ParseBig256("99943972190000000000")
-// var hundredEth, _ = math.ParseBig256("100000000000000000000")
+var firstAccountBalance, _ = math.ParseBig256("99943972190000000000")
+var hundredEth, _ = math.ParseBig256("100000000000000000000")
 
-// var pollingInterval = 100 * time.Millisecond
+var pollingInterval = 100 * time.Millisecond
 
-// func TestAddingAddressToETHWatcher(t *testing.T) {
-// 	ethClient, err := ethclient.Dial("http://localhost:8545")
-// 	if err != nil {
-// 		t.Fatal(err.Error())
-// 	}
-// 	ethWatcher := NewETHWatcher(pollingInterval, ethClient, config.GanacheEthBalanceCheckerAddress)
-
-// 	for address := range ethAccountToBalance {
-// 		ethWatcher.Add(address, big.NewInt(0))
-// 	}
-
-// 	addresses := []common.Address{}
-// 	for address := range ethWatcher.addressToBalance {
-// 		addresses = append(addresses, address)
-// 	}
-
-// 	expectedCount := 10
-// 	assert.Equal(t, expectedCount, len(addresses))
-// }
-
-// func TestUpdateBalancesETHWatcher(t *testing.T) {
-// 	ethClient, err := ethclient.Dial("http://localhost:8545")
-// 	if err != nil {
-// 		t.Fatal(err.Error())
-// 	}
-// 	ethWatcher := NewETHWatcher(pollingInterval, ethClient, config.GanacheEthBalanceCheckerAddress)
-
-// 	for address := range ethAccountToBalance {
-// 		ethWatcher.Add(address, big.NewInt(0))
-// 	}
-
-// 	go func() {
-// 		ethWatcher.updateBalances()
-// 	}()
-
-// 	for i := 0; i < len(ethAccountToBalance); i++ {
-// 		select {
-// 		case balance := <-ethWatcher.Receive():
-// 			assert.Equal(t, ethAccountToBalance[balance.Address], balance.Balance)
-
-// 		case <-time.After(3 * time.Second):
-// 			t.Fatal("Timed out waiting for balance channel to deliver expected balances")
-// 		}
-// 	}
-// }
-// func TestStartStopETHWatcher(t *testing.T) {
-// 	ethClient, err := ethclient.Dial("http://localhost:8545")
-// 	if err != nil {
-// 		t.Fatal(err.Error())
-// 	}
-// 	ethWatcher := NewETHWatcher(pollingInterval, ethClient, config.GanacheEthBalanceCheckerAddress)
-
-// 	for address := range ethAccountToBalance {
-// 		ethWatcher.Add(address, big.NewInt(0))
-// 	}
-
-// 	ethWatcher.Start()
-
-// 	for i := 0; i < len(ethAccountToBalance); i++ {
-// 		select {
-// 		case balance := <-ethWatcher.Receive():
-// 			assert.Equal(t, ethAccountToBalance[balance.Address], balance.Balance)
-
-// 		case <-time.After(3 * time.Second):
-// 			t.Fatal("Timed out waiting for balance channel to deliver expected balances")
-// 		}
-// 	}
-
-// 	ethWatcher.Stop()
-// }
-
-func TestEthClient(t *testing.T) {
-	fmt.Println("START!")
-	client := &http.Client{}
-
-	req, err := http.NewRequest("POST", "http://localhost:8545", nil)
+func TestAddingAddressToETHWatcher(t *testing.T) {
+	ethClient, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
-		panic(err)
+		t.Fatal(err.Error())
+	}
+	ethWatcher := NewETHWatcher(pollingInterval, ethClient, config.GanacheEthBalanceCheckerAddress)
+
+	for address := range ethAccountToBalance {
+		ethWatcher.Add(address, big.NewInt(0))
 	}
 
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/json")
-
-	msg := "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"eth_getBlockByNumber\",\"params\":[\"latest\",false]}"
-	req.Body = ioutil.NopCloser(bytes.NewReader([]byte(msg)))
-	req.ContentLength = int64(len(msg))
-
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
+	addresses := []common.Address{}
+	for address := range ethWatcher.addressToBalance {
+		addresses = append(addresses, address)
 	}
 
-	data, err := ioutil.ReadAll(resp.Body)
+	expectedCount := 10
+	assert.Equal(t, expectedCount, len(addresses))
+}
+
+func TestUpdateBalancesETHWatcher(t *testing.T) {
+	ethClient, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
-		panic(err)
+		t.Fatal(err.Error())
+	}
+	ethWatcher := NewETHWatcher(pollingInterval, ethClient, config.GanacheEthBalanceCheckerAddress)
+
+	for address := range ethAccountToBalance {
+		ethWatcher.Add(address, big.NewInt(0))
 	}
 
-	fmt.Println("data", data)
+	go func() {
+		ethWatcher.updateBalances()
+	}()
 
-	// ethClient, err := ethclient.Dial("http://localhost:8545")
-	// if err != nil {
-	// 	t.Fatal(err.Error())
-	// }
+	for i := 0; i < len(ethAccountToBalance); i++ {
+		select {
+		case balance := <-ethWatcher.Receive():
+			assert.Equal(t, ethAccountToBalance[balance.Address], balance.Balance)
 
-	// done := make(chan interface{})
+		case <-time.After(3 * time.Second):
+			t.Fatal("Timed out waiting for balance channel to deliver expected balances")
+		}
+	}
+}
+func TestStartStopETHWatcher(t *testing.T) {
+	ethClient, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	ethWatcher := NewETHWatcher(pollingInterval, ethClient, config.GanacheEthBalanceCheckerAddress)
 
-	// go func() {
-	// 	head, err := ethClient.HeaderByNumber(context.Background(), nil)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	fmt.Println("head", head)
-	// 	done <- 1
-	// }()
+	for address := range ethAccountToBalance {
+		ethWatcher.Add(address, big.NewInt(0))
+	}
 
-	// <-done
+	ethWatcher.Start()
+
+	for i := 0; i < len(ethAccountToBalance); i++ {
+		select {
+		case balance := <-ethWatcher.Receive():
+			assert.Equal(t, ethAccountToBalance[balance.Address], balance.Balance)
+
+		case <-time.After(3 * time.Second):
+			t.Fatal("Timed out waiting for balance channel to deliver expected balances")
+		}
+	}
+
+	ethWatcher.Stop()
 }
