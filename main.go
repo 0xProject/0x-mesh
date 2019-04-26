f7e7c642587dfdfe5d8b95173794ef611594887b
Fabio Berger <me@fabioberger.com>
2019-04-25 20:44:03 -0500

    Switch out flags for env vars

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"flag"
-
+	"github.com/plaid/go-envvar/envvar"
 	log "github.com/sirupsen/logrus"
 )
 
+type meshEnvVars struct {
+	// Logging verbosity: 0=panic, 1=fatal, 2=error, 3=warn, 4=info, 5=debug 6=trace
+	verbosity int `envvar:"VERBOSITY"`
+}
+
 func main() {
 	// TODO: Main entry point for the 0x Mesh node
-	var (
-		verbosity = flag.Int("verbosity", int(log.InfoLevel), "Logging verbosity: 0=panic, 1=fatal, 2=error, 3=warn, 4=info, 5=debug 6=trace")
-	)
-	flag.Parse()
+	vars := meshEnvVars{}
+	if err := envvar.Parse(&vars); err != nil {
+		log.Fatal(err)
+	}
 
 	// Logging settings
 	log.SetFormatter(&log.JSONFormatter{})
-	log.SetLevel(log.Level(*verbosity))
+	log.SetLevel(log.Level(vars.verbosity))
 }
