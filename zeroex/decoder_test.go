978608920d0f22920466df6a75504fe740d5e425
Fabio Berger <me@fabioberger.com>
2019-05-08 10:06:25 -0300

    Add missing import

--- zeroex/decoder_test.go
+++ zeroex/decoder_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDecodeERC20AssetData(t *testing.T) {
