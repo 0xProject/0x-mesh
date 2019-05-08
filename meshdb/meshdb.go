8fbc696214008b2d8f175b6a20d2aec634f13551
Fabio Berger <me@fabioberger.com>
2019-05-08 11:29:05 -0300

    Add Orders collection to MeshDB and a few preliminary tests

--- meshdb/meshdb.go
+++ meshdb/meshdb.go
@@ -3,10 +3,13 @@ package meshdb
 import (
 	"fmt"
 	"math/big"
+	"time"
 
 	"github.com/0xProject/0x-mesh/db"
+	"github.com/0xProject/0x-mesh/zeroex"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	log "github.com/sirupsen/logrus"
 )
 
 // MiniHeader is the database representation of a succinct Ethereum block headers
@@ -22,12 +25,30 @@ func (m *MiniHeader) ID() []byte {
 	return m.Hash.Bytes()
 }
 
-// MeshDB instantiates the DB connection and creates all the collections used by the application
-type MeshDB struct {
-	database    *db.DB
-	MiniHeaders *MiniHeadersCollection
+// Order is the database representation a 0x order along with some relevant metadata
+type Order struct {
+	Hash        common.Hash
+	SignedOrder *zeroex.SignedOrder
+	// When was this order last validated
+	LastUpdated time.Time
+	// How much of this order can still be filled
+	FillableTakerAssetAmount *big.Int
 }
 
+// ID returns the Order's ID
+func (o Order) ID() []byte {
+	return o.Hash.Bytes()
+}
+
+// MeshDB instantiates the DB connection and creates all the collections used by the application
+type MeshDB struct {
+	database                             *db.DB
+	MiniHeaders                          *MiniHeadersCollection
+	Orders                               *db.Collection
+	makerAddressTokenAddressTokenIDIndex *db.Index
+}
+
+// MiniHeadersCollection represents a DB collection of mini Ethereum block headers
 type MiniHeadersCollection struct {
 	*db.Collection
 	numberIndex *db.Index
@@ -45,10 +66,37 @@ func NewMeshDB(path string) (*MeshDB, error) {
 		return nil, err
 	}
 
+	orders, makerAddressTokenAddressTokenIDIndex := setupOrders(database)
+
 	return &MeshDB{
-		database:    database,
-		MiniHeaders: miniHeaders,
+		database:                             database,
+		MiniHeaders:                          miniHeaders,
+		Orders:                               orders,
+		makerAddressTokenAddressTokenIDIndex: makerAddressTokenAddressTokenIDIndex,
 	}, nil
+}
+
+func setupOrders(database *db.DB) (*db.Collection, *db.Index) {
+	orders := database.NewCollection("order", &Order{})
+	orders.AddIndex("salt", func(m db.Model) []byte {
+		return []byte(fmt.Sprint(m.(*Order).SignedOrder.Salt))
+	})
+	makerAddressTokenAddressTokenIDIndex := orders.AddMultiIndex("makerAddressTokenAddressTokenId", func(m db.Model) [][]byte {
+		order := m.(*Order)
+		singleAssetDatas, err := parseContractAddressesAndTokenIdsFromAssetData(order.SignedOrder.MakerAssetData)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err.Error(),
+			}).Panic("Parsing assetData failed")
+		}
+
+		indexValues := make([][]byte, len(singleAssetDatas))
+		for i, singleAssetData := range singleAssetDatas {
+			indexValues[i] = []byte(fmt.Sprintf(order.SignedOrder.MakerAddress.Hex(), "|", singleAssetData.Address.Hex(), "|", singleAssetData.TokenID))
+		}
+		return indexValues
+	})
+	return orders, makerAddressTokenAddressTokenIDIndex
 }
 
 func setupMiniHeaders(database *db.DB) (*MiniHeadersCollection, error) {
@@ -97,4 +145,61 @@ func (m *MeshDB) FindLatestMiniHeader() (*MiniHeader, error) {
 		return nil, nil
 	}
 	return miniHeaders[0], nil
+}
+
+type singleAssetData struct {
+	Address common.Address
+	TokenID *big.Int
+}
+
+func parseContractAddressesAndTokenIdsFromAssetData(assetData []byte) ([]singleAssetData, error) {
+	singleAssetDatas := []singleAssetData{}
+	assetDataDecoder, err := zeroex.NewAssetDataDecoder()
+	if err != nil {
+		return nil, err
+	}
+
+	assetDataName, err := assetDataDecoder.GetName(assetData)
+	if err != nil {
+		return nil, err
+	}
+	switch assetDataName {
+	case "ERC20Token":
+		var decodedAssetData zeroex.ERC20AssetData
+		err := assetDataDecoder.Decode(assetData, &decodedAssetData)
+		if err != nil {
+			return nil, err
+		}
+		a := singleAssetData{
+			Address: decodedAssetData.Address,
+		}
+		singleAssetDatas = append(singleAssetDatas, a)
+	case "ERC721Token":
+		var decodedAssetData zeroex.ERC721AssetData
+		err := assetDataDecoder.Decode(assetData, &decodedAssetData)
+		if err != nil {
+			return nil, err
+		}
+		a := singleAssetData{
+			Address: decodedAssetData.Address,
+			TokenID: decodedAssetData.TokenId,
+		}
+		singleAssetDatas = append(singleAssetDatas, a)
+	case "MultiAsset":
+		var decodedAssetData zeroex.MultiAssetData
+		err := assetDataDecoder.Decode(assetData, &decodedAssetData)
+		if err != nil {
+			return nil, err
+		}
+		for _, assetData := range decodedAssetData.NestedAssetData {
+			as, err := parseContractAddressesAndTokenIdsFromAssetData(assetData)
+			if err != nil {
+				return nil, err
+			}
+			singleAssetDatas = append(singleAssetDatas, as...)
+		}
+	default:
+		return nil, fmt.Errorf("unrecognized assetData type name found: %s", assetDataName)
+	}
+	return singleAssetDatas, nil
 }
---
+++ meshdb/meshdb_test.go
@@ -0,0 +1,106 @@
+package meshdb
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/0xProject/0x-mesh/zeroex"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var nullAddress = common.HexToAddress("0x0000000000000000000000000000000000000000")
+var ganacheExchangeAddress = common.HexToAddress("0x48bacb9266a570d521063ef5dd96e61686dbe788")
+
+func TestOrderCRUDOperations(t *testing.T) {
+	meshDB, err := NewMeshDB("/tmp/meshdb_testing/" + uuid.New().String())
+	require.NoError(t, err)
+
+	makerAddress := common.HexToAddress("0x6924a03bb710eaf199ab6ac9f2bb148215ae9b5d")
+	// makerTokenAddress := common.HexToAddress("0x34d402f14d58e001d8efbe6585051bf9706aa064")
+	signedOrder := &zeroex.SignedOrder{
+		MakerAddress:          makerAddress,
+		TakerAddress:          nullAddress,
+		SenderAddress:         nullAddress,
+		FeeRecipientAddress:   common.HexToAddress("0xa258b39954cef5cb142fd567a46cddb31a670124"),
+		MakerAssetData:        common.Hex2Bytes("f47261b000000000000000000000000034d402f14d58e001d8efbe6585051bf9706aa064"),
+		TakerAssetData:        common.Hex2Bytes("025717920000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c480000000000000000000000000000000000000000000000000000000000000001"),
+		Salt:                  big.NewInt(1548619145450),
+		MakerFee:              big.NewInt(0),
+		TakerFee:              big.NewInt(0),
+		MakerAssetAmount:      big.NewInt(3551808554499581700),
+		TakerAssetAmount:      big.NewInt(1),
+		ExpirationTimeSeconds: big.NewInt(1548619325),
+		ExchangeAddress:       ganacheExchangeAddress,
+	}
+	orderHash, err := signedOrder.ComputeOrderHash()
+	require.NoError(t, err)
+
+	// Insert
+	order := &Order{
+		Hash:                     orderHash,
+		SignedOrder:              signedOrder,
+		FillableTakerAssetAmount: big.NewInt(1),
+	}
+	require.NoError(t, meshDB.Orders.Insert(order))
+
+	// Find
+	foundOrder := &Order{}
+	require.NoError(t, meshDB.Orders.FindByID(order.ID(), foundOrder))
+	assert.Equal(t, order, foundOrder)
+
+	// Check Indexes [NOT WORKING]
+	// prefix := []byte(fmt.Sprintf(makerAddress.Hex(), "|"))
+	// filter := meshDB.makerAddressTokenAddressTokenIDIndex.PrefixFilter(prefix)
+	// foundOrder = &Order{}
+	// meshDB.Orders.NewQuery(filter).Run(foundOrder)
+	// assert.Equal(t, order, foundOrder)
+
+	// Update
+
+	// Delete
+}
+
+func TestParseContractAddressesAndTokenIdsFromAssetData(t *testing.T) {
+	// ERC20 AssetData
+	erc20AssetData := common.Hex2Bytes("f47261b000000000000000000000000038ae374ecf4db50b0ff37125b591a04997106a32")
+	singleAssetDatas, err := parseContractAddressesAndTokenIdsFromAssetData(erc20AssetData)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(singleAssetDatas))
+	expectedAddress := common.HexToAddress("0x38ae374ecf4db50b0ff37125b591a04997106a32")
+	assert.Equal(t, expectedAddress, singleAssetDatas[0].Address)
+	var expectedTokenID *big.Int
+	assert.Equal(t, expectedTokenID, singleAssetDatas[0].TokenID)
+
+	// ERC721 AssetData
+	erc721AssetData := common.Hex2Bytes("025717920000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c480000000000000000000000000000000000000000000000000000000000000001")
+	singleAssetDatas, err = parseContractAddressesAndTokenIdsFromAssetData(erc721AssetData)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(singleAssetDatas))
+	expectedAddress = common.HexToAddress("0x1dC4c1cEFEF38a777b15aA20260a54E584b16C48")
+	assert.Equal(t, expectedAddress, singleAssetDatas[0].Address)
+	expectedTokenID = big.NewInt(1)
+	assert.Equal(t, expectedTokenID, singleAssetDatas[0].TokenID)
+
+	// Multi AssetData
+	multiAssetData := common.Hex2Bytes("94cfcdd7000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004600000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000024f47261b00000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c48000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044025717920000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c480000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000x94cfcdd7000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004600000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000024f47261b00000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c48000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044025717920000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c48000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000")
+	singleAssetDatas, err = parseContractAddressesAndTokenIdsFromAssetData(multiAssetData)
+	require.NoError(t, err)
+	assert.Equal(t, 2, len(singleAssetDatas))
+	expectedSingleAssetDatas := []singleAssetData{
+		singleAssetData{
+			Address: common.HexToAddress("0x1dc4c1cefef38a777b15aa20260a54e584b16c48"),
+		},
+		singleAssetData{
+			Address: common.HexToAddress("0x1dc4c1cefef38a777b15aa20260a54e584b16c48"),
+			TokenID: big.NewInt(1),
+		},
+	}
+	for i, singleAssetData := range singleAssetDatas {
+		expectedSingleAssetData := expectedSingleAssetDatas[i]
+		assert.Equal(t, expectedSingleAssetData.Address, singleAssetData.Address)
+		assert.Equal(t, expectedSingleAssetData.TokenID, singleAssetData.TokenID)
+	}
+}
