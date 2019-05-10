2c29de4837bbbb3327e63063830e33bf960b1344
Fabio Berger <me@fabioberger.com>
2019-05-09 22:38:31 -0300

    Use type-aliases in a separate files and undo the edits to the auto-generated contract wrapper file

---
+++ ethereum/wrappers/order_validator_aliases.go
@@ -0,0 +1,10 @@
+package wrappers
+
+// OrderWithoutExchangeAddress is a 0x order representation expected by the smart contracts.
+type OrderWithoutExchangeAddress = Struct1
+
+// TraderInfo contains all the order-relevant trader balances & allowances.
+type TraderInfo = Struct2
+
+// OrderInfo contains the status and filled amount of an order.
+type OrderInfo = Struct3
