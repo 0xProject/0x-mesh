27fb00a04779841672893744b718411c4bad2f91
Fabio Berger <me@fabioberger.com>
2019-05-09 22:39:41 -0300

    Add a HACK comment above the type aliases

--- ethereum/wrappers/order_validator_aliases.go
+++ ethereum/wrappers/order_validator_aliases.go
@@ -1,5 +1,8 @@
 package wrappers
 
+// HACK(fabio): The `abi-gen` tool is unable to properly name the structs in generated from the
+// contract ABI so we add type aliases here to correct the struct names.
+
 // OrderWithoutExchangeAddress is a 0x order representation expected by the smart contracts.
 type OrderWithoutExchangeAddress = Struct1
 
