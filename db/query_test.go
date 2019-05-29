e49e6b9b21c5248d256c3b835cb5db9e7b13a272
Alex Browne <stephenalexbrowne@gmail.com>
2019-05-29 16:43:07 -0700

    Test queries with both Reverse and Offset

--- db/query_test.go
+++ db/query_test.go
@@ -287,6 +287,14 @@ func testQueryWithFilter(t *testing.T, col *Collection, filter *Filter, expected
 			query:    col.NewQuery(filter).Offset(1).Max(safeMax - 1),
 			expected: expected[1:safeMax],
 		},
+		{
+			query:    col.NewQuery(filter).Offset(1).Reverse(),
+			expected: reverseExpected[1:],
+		},
+		{
+			query:    col.NewQuery(filter).Offset(1).Max(safeMax - 1).Reverse(),
+			expected: reverseExpected[1:safeMax],
+		},
 	}
 
 	for i, tc := range testCases {
