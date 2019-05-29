0db1f82b355f230ee98965893cb41dc2e77ec107
Alex Browne <stephenalexbrowne@gmail.com>
2019-05-28 18:03:07 -0700

    Move encodeOrder and decodeOrder for Wasm compatibility

--- core/encoding.go
+++ core/encoding.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/0xProject/0x-mesh/p2p"
@@ -12,6 +13,29 @@ import (
 const maxSizeInBytes = 8192
 
 var errMaxSize = fmt.Errorf("message exceeds maximum size of %d bytes", maxSizeInBytes)
+
+type orderMessage struct {
+	MessageType string
+	Order       *zeroex.SignedOrder
+}
+
+func encodeOrder(order *zeroex.SignedOrder) ([]byte, error) {
+	return json.Marshal(orderMessage{
+		MessageType: "order",
+		Order:       order,
+	})
+}
+
+func decodeOrder(data []byte) (*zeroex.SignedOrder, error) {
+	var orderMessage orderMessage
+	if err := json.Unmarshal(data, &orderMessage); err != nil {
+		return nil, err
+	}
+	if orderMessage.MessageType != "order" {
+		return nil, fmt.Errorf("unexpected message type: %q", orderMessage.MessageType)
+	}
+	return orderMessage.Order, nil
+}
 
 func validateMessageSize(message *p2p.Message) error {
 	if len(message.Data) > maxSizeInBytes {
--- core/message_handler.go
+++ core/message_handler.go
@@ -3,8 +3,6 @@
 package core
 
 import (
-	"encoding/json"
-	"fmt"
 	"math/rand"
 
 	"github.com/0xProject/0x-mesh/meshdb"
@@ -13,29 +11,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	log "github.com/sirupsen/logrus"
 )
-
-type orderMessage struct {
-	MessageType string
-	Order       *zeroex.SignedOrder
-}
-
-func encodeOrder(order *zeroex.SignedOrder) ([]byte, error) {
-	return json.Marshal(orderMessage{
-		MessageType: "order",
-		Order:       order,
-	})
-}
-
-func decodeOrder(data []byte) (*zeroex.SignedOrder, error) {
-	var orderMessage orderMessage
-	if err := json.Unmarshal(data, &orderMessage); err != nil {
-		return nil, err
-	}
-	if orderMessage.MessageType != "order" {
-		return nil, fmt.Errorf("unexpected message type: %q", orderMessage.MessageType)
-	}
-	return orderMessage.Order, nil
-}
 
 // Ensure that App implements p2p.MessageHandler.
 var _ p2p.MessageHandler = &App{}
