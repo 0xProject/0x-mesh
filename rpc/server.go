09a0b3aa07cc2d3d80e9a6ff79b3389971896fad
Alex Browne <stephenalexbrowne@gmail.com>
2019-05-30 16:22:59 -0700

    Make all error logs use the "error" key

--- rpc/server.go
+++ rpc/server.go
@@ -42,12 +42,12 @@ func (s *Server) Listen() error {
 	}
 	s.rpcServer = rpc.NewServer()
 	if err := s.rpcServer.RegisterName("mesh", rpcService); err != nil {
-		log.WithField("err", err.Error()).Fatal("could not register RPC service")
+		log.WithField("error", err.Error()).Fatal("could not register RPC service")
 	}
 	listener, err := net.Listen("tcp4", s.addr)
 	if err != nil {
 		s.mut.Unlock()
-		log.WithField("err", err.Error()).Fatal("could not start listener")
+		log.WithField("error", err.Error()).Fatal("could not start listener")
 	}
 	s.listener = listener
 	s.mut.Unlock()
