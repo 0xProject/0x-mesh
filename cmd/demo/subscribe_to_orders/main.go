40ab6f119a089c01da412856561a25c39b5271b3
Alex Browne <stephenalexbrowne@gmail.com>
2019-05-30 16:46:00 -0700

    Remove unneeded env var from subscribe_to_orders script

--- cmd/demo/subscribe_to_orders/main.go
+++ cmd/demo/subscribe_to_orders/main.go
@@ -15,9 +15,6 @@ import (
 type clientEnvVars struct {
 	// RPCAddress is the address of the 0x Mesh node to communicate with.
 	RPCAddress string `envvar:"RPC_ADDRESS"`
-	// EthereumRPCURL is the URL of an Etheruem node which supports the JSON RPC
-	// API.
-	EthereumRPCURL string `envvar:"ETHEREUM_RPC_URL"`
 }
 
 func main() {
