95b7586ae24a4a3aded65892c9f426ebf7e16f80
Fabio Berger <me@fabioberger.com>
2019-06-01 23:11:51 +0100

    Fix import

--- zeroex/order.go
+++ zeroex/order.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/0xProject/0x-mesh/ethereum"
 	"github.com/0xProject/0x-mesh/ethereum/wrappers"
-	"github.com/0xProject/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/math"
 	signer "github.com/ethereum/go-ethereum/signer/core"
 	"golang.org/x/crypto/sha3"
 )
