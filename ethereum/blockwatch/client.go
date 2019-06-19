5c5bb5323f5bafc2348c7701c8f9145eec8ce478
Fabio B <kandinsky454@protonmail.ch>
2019-06-19 16:59:08 -0400

    Update ethereum/blockwatch/client.go
    Co-Authored-By: Alex Browne <stephenalexbrowne@gmail.com>

--- ethereum/blockwatch/client.go
+++ ethereum/blockwatch/client.go
@@ -68,7 +68,7 @@ func (rc *RpcClient) HeaderByNumber(number *big.Int) (*meshdb.MiniHeader, error)
 	// Note(fabio): We use a raw RPC call here instead of `EthClient`'s `BlockByNumber()` method because block
 	// hashes are computed differently on Kovan vs. mainnet, resulting in the wrong block hash being returned by
 	// `BlockByNumber` when using Kovan. By doing a raw RPC call, we can simply use the blockHash returned in the
-	// RPC response rather then re-compute it from the block header.
+	// RPC response rather than re-compute it from the block header.
 	// Source: https://github.com/ethereum/go-ethereum/pull/18166
 	var header GetBlockByNumberResponse
 	err := rc.rpcClient.CallContext(ctx, &header, "eth_getBlockByNumber", blockParam, shouldIncludeTransactions)
