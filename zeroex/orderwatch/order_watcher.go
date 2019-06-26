47745b879ba5d2a3fea5032745c859ef2597dc10
fabioberger <me@fabioberger.com>
2019-06-26 23:18:25 +0200

    Remove superfluous ok return value

--- zeroex/orderwatch/order_watcher.go
+++ zeroex/orderwatch/order_watcher.go
@@ -410,8 +410,8 @@ func (w *Watcher) setupEventWatcher() {
 								continue
 							}
 							orders = []*meshdb.Order{}
-							order, ok := w.findOrderAndGenerateOrderEvents(exchangeFillEvent.OrderHash)
-							if ok {
+							order := w.findOrderAndGenerateOrderEvents(exchangeFillEvent.OrderHash)
+							if order != nil {
 								orders = append(orders, order)
 							}
 
@@ -423,8 +423,8 @@ func (w *Watcher) setupEventWatcher() {
 								continue
 							}
 							orders = []*meshdb.Order{}
-							order, ok := w.findOrderAndGenerateOrderEvents(exchangeCancelEvent.OrderHash)
-							if ok {
+							order := w.findOrderAndGenerateOrderEvents(exchangeCancelEvent.OrderHash)
+							if order != nil {
 								orders = append(orders, order)
 							}
 
@@ -468,21 +468,21 @@ func (w *Watcher) setupEventWatcher() {
 	}()
 }
 
-func (w *Watcher) findOrderAndGenerateOrderEvents(orderHash common.Hash) (*meshdb.Order, bool) {
+func (w *Watcher) findOrderAndGenerateOrderEvents(orderHash common.Hash) (*meshdb.Order) {
 	order := meshdb.Order{}
 	err := w.meshDB.Orders.FindByID(orderHash.Bytes(), &order)
 	if err != nil {
 		if _, ok := err.(db.NotFoundError); ok {
 			// short-circuit. We expect to receive events from orders we aren't actively tracking
-			return nil, false
+			return nil
 		}
 		logger.WithFields(logger.Fields{
 			"error":     err.Error(),
 			"orderHash": orderHash,
 		}).Warning("Unexpected error using FindByID for order")
-		return nil, false
+		return nil
 	}
-	return &order, true
+	return &order
 }
 
 func (w *Watcher) findOrdersAndGenerateOrderEvents(makerAddress, tokenAddress common.Address, tokenID *big.Int) []*meshdb.Order {
