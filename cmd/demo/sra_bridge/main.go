ae67f41ce8387d38e5841958374ac9b6a5152f6e
fabioberger <me@fabioberger.com>
2019-07-10 16:59:14 +0200

    Update SRA script

--- cmd/demo/sra_bridge/main.go
+++ cmd/demo/sra_bridge/main.go
@@ -52,7 +52,11 @@ func main() {
 			case orderEvents := <-orderEventsChan:
 				for _, orderEvent := range orderEvents {
 					if orderEvent.Kind != "ADDED" && orderEvent.Kind != "EXPIRED" {
-						log.Printf("%s - %s - remaining: %d -- txHash %s\n", orderEvent.Kind, orderEvent.OrderHash.Hex(), orderEvent.FillableTakerAssetAmount, orderEvent.TxHash.Hex())
+						txHashes := []string{}
+						for _, txHash := range orderEvent.TxHashes {
+							txHashes = append(txHashes, txHash.Hex())
+						}
+						log.Printf("%s - %s - remaining: %d -- txHash %s\n", orderEvent.Kind, orderEvent.OrderHash.Hex(), orderEvent.FillableTakerAssetAmount, txHashes)
 					}
 				}
 			}
@@ -62,7 +66,7 @@ func main() {
 	}()
 
 	ticker := time.NewTicker(20 * time.Second)
-	chunkSize := 200
+	chunkSize := 510
 	for {
 		signedOrders := getRadarOrders()
 		// signedOrderJSON := `{"exchangeAddress":"0x4530c0483a1633c7a1c97d2c53721caff2caaaaf","makerAddress":"0x8cff49b26d4d13e0601769f8a60fd697b713b9c6","takerAddress":"0x0000000000000000000000000000000000000000","senderAddress":"0x0000000000000000000000000000000000000000","feeRecipientAddress":"0x0000000000000000000000000000000000000000","expirationTimeSeconds":"1559826927","salt":"48128453606684653105952683301312821720867493716494911784363103883716429240740","makerAssetAmount":"100000000000000000","takerAssetAmount":"1000000000000000000","takerAssetData":"0xf47261b0000000000000000000000000ff67881f8d12f372d91baae9752eb3631ff0ed00","makerAssetData":"0xf47261b0000000000000000000000000c778417e063141139fce010982780140aa0cd5ab","makerFee":"0","takerFee":"0","signature":"0x1cf5839d9a0025e684c3663151b1db14533cc8c9e495fb92543a37a7fffc0677a23f3b6d66a1f56d3fda46eb5277b4a91c7b7faad4fdaaa5aac9a1185dd545a8a002"}`
