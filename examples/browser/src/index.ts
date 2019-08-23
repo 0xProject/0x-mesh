b5c7cfcc78225c366f9046d701bb2964eb0ca1da
Alex Browne <stephenalexbrowne@gmail.com>
2019-08-23 11:10:37 -0700

    Update examples/browser/src/index.ts
    Co-Authored-By: Fabio B <me@fabioberger.com>

--- examples/browser/src/index.ts
+++ examples/browser/src/index.ts
@@ -1,7 +1,7 @@
 import { Mesh, OrderEvent, SignedOrder, BigNumber } from '@0x/mesh-browser';
 
 (async () => {
-    // Confingure Mesh to use mainnet and Infura.
+    // Configure Mesh to use mainnet and Infura.
     const mesh = new Mesh({
         ethereumRPCURL: 'https://mainnet.infura.io/v3/af2e590be00f463fbfd0b546784065ad',
         ethereumNetworkID: 1,
