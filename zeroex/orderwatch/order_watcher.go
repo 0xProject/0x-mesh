96e5b52f87cc3e934eaabba83cf62d6619569096
fabioberger <me@fabioberger.com>
2019-09-03 17:19:55 +0200

    Update both the lastUpdated and fillableTakerAssetAmount in DB

--- zeroex/orderwatch/order_watcher.go
+++ zeroex/orderwatch/order_watcher.go
@@ -612,11 +612,11 @@ func (w *Watcher) generateOrderEventsIfChanged(hashToOrderWithTxHashes map[commo
 			}
 			orderEvents = append(orderEvents, orderEvent)
 		} else if oldFillableAmount.Cmp(newFillableAmount) == 0 {
-			// No important state-change happened, simply update lastUpdated timestamp
-			w.actualizeLastUpdated(order)
+			// No important state-change happened, simply update lastUpdated timestamp in DB
+			w.updateOrderLastUpdatedAndFillableAmount(order)
 		} else if oldFillableAmount.Cmp(big.NewInt(0)) == 1 && oldAmountIsMoreThenNewAmount {
-			w.actualizeLastUpdated(order)
-			// Order was filled, emit  event
+			// Order was filled, emit  event and update order in DB
+			w.updateOrderLastUpdatedAndFillableAmount(order)
 			orderEvent := &zeroex.OrderEvent{
 				OrderHash:                acceptedOrderInfo.OrderHash,
 				SignedOrder:              order.SignedOrder,
@@ -626,9 +626,9 @@ func (w *Watcher) generateOrderEventsIfChanged(hashToOrderWithTxHashes map[commo
 			}
 			orderEvents = append(orderEvents, orderEvent)
 		} else if oldFillableAmount.Cmp(big.NewInt(0)) == 1 && !oldAmountIsMoreThenNewAmount {
-			w.actualizeLastUpdated(order)
-			// The order is now fillable for more then it was before. E.g.:
-			// 1. A fill txn reverted (block-reorg)
+			// The order is now fillable for more then it was before. E.g.: A fill txn reverted (block-reorg)
+			// Update order in DB and emit event
+			w.updateOrderLastUpdatedAndFillableAmount(order)
 			orderEvent := &zeroex.OrderEvent{
 				OrderHash:                acceptedOrderInfo.OrderHash,
 				SignedOrder:              order.SignedOrder,
@@ -648,9 +648,9 @@ func (w *Watcher) generateOrderEventsIfChanged(hashToOrderWithTxHashes map[commo
 			order := orderWithTxHashes.Order
 			oldFillableAmount := order.FillableTakerAssetAmount
 			if oldFillableAmount.Cmp(big.NewInt(0)) == 0 {
-				// If the oldFillableAmount was already 0, this order is already flagged for removal. We simply
-				// update it's lastUpdated timestamp
-				w.actualizeLastUpdated(order)
+				// If the oldFillableAmount was already 0, this order is already flagged for removal.
+				// Update it's lastUpdated timestamp in DB
+				w.updateOrderLastUpdatedAndFillableAmount(order)
 			} else {
 				// If oldFillableAmount > 0, it got fullyFilled, cancelled, expired or unfunded, emit event
 				w.unwatchOrder(order, big.NewInt(0))
@@ -687,8 +687,9 @@ func (w *Watcher) generateOrderEventsIfChanged(hashToOrderWithTxHashes map[commo
 	return nil
 }
 
-func (w *Watcher) actualizeLastUpdated(order *meshdb.Order) {
+func (w *Watcher) updateOrderLastUpdatedAndFillableAmount(order *meshdb.Order) {
 	order.LastUpdated = time.Now().UTC()
+	order.FillableTakerAssetAmount = order.FillableTakerAssetAmount
 	err := w.meshDB.Orders.Update(order)
 	if err != nil {
 		logger.WithFields(logger.Fields{
