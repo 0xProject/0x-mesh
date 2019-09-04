01437f494aa929c0b2aa85235f138f1821be8344
fabioberger <me@fabioberger.com>
2019-09-04 16:45:38 +0200

    Add HACK comment above MiniHeader's `ID()` method

--- ethereum/miniheader/miniheader.go
+++ ethereum/miniheader/miniheader.go
@@ -16,6 +16,10 @@ type MiniHeader struct {
 }
 
 // ID returns the MiniHeader's ID
+// HACK(fabio): This method is only used by DBStack and not SimpleStack
+// Ideally this would live in the `meshdb` package but it adds the need
+// to cast back-and-forth between two almost identical types so we keep
+// it here for convenience sake.
 func (m *MiniHeader) ID() []byte {
 	return m.Hash.Bytes()
 }
