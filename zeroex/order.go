d6dcd329f42c686c0a51f8dad0558346b84adc46
Alex Browne <stephenalexbrowne@gmail.com>
2019-09-24 12:05:31 -0700

    Rename verifyingContractAddress to verifyingContract

--- zeroex/order.go
+++ zeroex/order.go
@@ -200,7 +200,7 @@ var eip712OrderTypes = signer.Types{
 			Type: "uint256",
 		},
 		{
-			Name: "verifyingContractAddress",
+			Name: "verifyingContract",
 			Type: "address",
 		},
 	},
@@ -280,9 +280,9 @@ func (o *Order) ComputeOrderHash() (common.Hash, error) {
 		return *o.hash, err
 	}
 	var domain = signer.TypedDataDomain{
-		Name:                     "0x Protocol",
-		Version:                  "2",
-		VerifyingContractAddress: exchangeAddress.Hex(),
+		Name:              "0x Protocol",
+		Version:           "2",
+		VerifyingContract: exchangeAddress.Hex(),
 	}
 
 	var message = map[string]interface{}{
