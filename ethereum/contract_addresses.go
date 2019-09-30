84d63c8388cbd22bcac74855ac80873979085295
Alex Browne <stephenalexbrowne@gmail.com>
2019-09-30 14:47:41 -0700

    Update DevUtils address on Kovan

--- ethereum/contract_addresses.go
+++ ethereum/contract_addresses.go
@@ -41,39 +41,40 @@ type ContractAddresses struct {
 // NetworkIDToContractAddresses maps networkId to a mapping of contract name to Ethereum address
 // on that given network
 var NetworkIDToContractAddresses = map[int]ContractAddresses{
-	// Mainnet
-	1: ContractAddresses{
-		ERC20Proxy:          common.HexToAddress("0x95e6f48254609a6ee006f7d493c8e5fb97094cef"),
-		ERC721Proxy:         common.HexToAddress("0xefc70a1b18c432bdc64b596838b4d138f6bc6cad"),
-		Exchange:            common.HexToAddress("0x080bf510fcbf18b91105470639e9561022937712"),
-		Coordinator:         common.HexToAddress("0xa14857e8930acd9a882d33ec20559beb5479c8a6"),
-		CoordinatorRegistry: common.HexToAddress("0x45797531b873fd5e519477a070a955764c1a5b07"),
-		DevUtils:            common.HexToAddress("0x92d9a4d50190ae04e03914db2ee650124af844e6"),
-		WETH9:               common.HexToAddress("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"),
-		ZRXToken:            common.HexToAddress("0xe41d2489571d322189246dafa5ebde1f4699f498"),
-	},
-	// Ropsten
-	3: ContractAddresses{
-		ERC20Proxy:          common.HexToAddress("0xb1408f4c245a23c31b98d2c626777d4c0d766caa"),
-		ERC721Proxy:         common.HexToAddress("0xe654aac058bfbf9f83fcaee7793311dd82f6ddb4"),
-		Exchange:            common.HexToAddress("0xbff9493f92a3df4b0429b6d00743b3cfb4c85831"),
-		Coordinator:         common.HexToAddress("0x2ba02e03ee0029311e0f43715307870a3e701b53"),
-		CoordinatorRegistry: common.HexToAddress("0x403cc23e88c17c4652fb904784d1af640a6722d9"),
-		DevUtils:            common.HexToAddress("0x3e0b46bad8e374e4a110c12b832cb120dbe4a479"),
-		WETH9:               common.HexToAddress("0xc778417e063141139fce010982780140aa0cd5ab"),
-		ZRXToken:            common.HexToAddress("0xff67881f8d12f372d91baae9752eb3631ff0ed00"),
-	},
-	// Rinkeby
-	4: ContractAddresses{
-		ERC20Proxy:          common.HexToAddress("0x3e809c563c15a295e832e37053798ddc8d6c8dab"),
-		ERC721Proxy:         common.HexToAddress("0x8e1ff02637cb5e39f2fa36c14706aa348b065b09"),
-		Exchange:            common.HexToAddress("0xbff9493f92a3df4b0429b6d00743b3cfb4c85831"),
-		Coordinator:         common.HexToAddress("0x2ba02e03ee0029311e0f43715307870a3e701b53"),
-		CoordinatorRegistry: common.HexToAddress("0x1084b6a398e47907bae43fec3ff4b677db6e4fee"),
-		DevUtils:            common.HexToAddress("0x2d4a9abda7b8b3605c8dbd34e3550a7467c78287"),
-		WETH9:               common.HexToAddress("0xc778417e063141139fce010982780140aa0cd5ab"),
-		ZRXToken:            common.HexToAddress("0x2727e688b8fd40b198cd5fe6e408e00494a06f07"),
-	},
+	// TODO(albrow): Uncomment these after we have deployed V3 to each network.
+	// // Mainnet
+	// 1: ContractAddresses{
+	// 	ERC20Proxy:          common.HexToAddress("0x95e6f48254609a6ee006f7d493c8e5fb97094cef"),
+	// 	ERC721Proxy:         common.HexToAddress("0xefc70a1b18c432bdc64b596838b4d138f6bc6cad"),
+	// 	Exchange:            common.HexToAddress("0x080bf510fcbf18b91105470639e9561022937712"),
+	// 	Coordinator:         common.HexToAddress("0xa14857e8930acd9a882d33ec20559beb5479c8a6"),
+	// 	CoordinatorRegistry: common.HexToAddress("0x45797531b873fd5e519477a070a955764c1a5b07"),
+	// 	DevUtils:            common.HexToAddress("0x92d9a4d50190ae04e03914db2ee650124af844e6"),
+	// 	WETH9:               common.HexToAddress("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"),
+	// 	ZRXToken:            common.HexToAddress("0xe41d2489571d322189246dafa5ebde1f4699f498"),
+	// },
+	// // Ropsten
+	// 3: ContractAddresses{
+	// 	ERC20Proxy:          common.HexToAddress("0xb1408f4c245a23c31b98d2c626777d4c0d766caa"),
+	// 	ERC721Proxy:         common.HexToAddress("0xe654aac058bfbf9f83fcaee7793311dd82f6ddb4"),
+	// 	Exchange:            common.HexToAddress("0xbff9493f92a3df4b0429b6d00743b3cfb4c85831"),
+	// 	Coordinator:         common.HexToAddress("0x2ba02e03ee0029311e0f43715307870a3e701b53"),
+	// 	CoordinatorRegistry: common.HexToAddress("0x403cc23e88c17c4652fb904784d1af640a6722d9"),
+	// 	DevUtils:            common.HexToAddress("0x3e0b46bad8e374e4a110c12b832cb120dbe4a479"),
+	// 	WETH9:               common.HexToAddress("0xc778417e063141139fce010982780140aa0cd5ab"),
+	// 	ZRXToken:            common.HexToAddress("0xff67881f8d12f372d91baae9752eb3631ff0ed00"),
+	// },
+	// // Rinkeby
+	// 4: ContractAddresses{
+	// 	ERC20Proxy:          common.HexToAddress("0x3e809c563c15a295e832e37053798ddc8d6c8dab"),
+	// 	ERC721Proxy:         common.HexToAddress("0x8e1ff02637cb5e39f2fa36c14706aa348b065b09"),
+	// 	Exchange:            common.HexToAddress("0xbff9493f92a3df4b0429b6d00743b3cfb4c85831"),
+	// 	Coordinator:         common.HexToAddress("0x2ba02e03ee0029311e0f43715307870a3e701b53"),
+	// 	CoordinatorRegistry: common.HexToAddress("0x1084b6a398e47907bae43fec3ff4b677db6e4fee"),
+	// 	DevUtils:            common.HexToAddress("0x2d4a9abda7b8b3605c8dbd34e3550a7467c78287"),
+	// 	WETH9:               common.HexToAddress("0xc778417e063141139fce010982780140aa0cd5ab"),
+	// 	ZRXToken:            common.HexToAddress("0x2727e688b8fd40b198cd5fe6e408e00494a06f07"),
+	// },
 	// Kovan
 	42: ContractAddresses{
 		ERC20Proxy:          common.HexToAddress("0xf1ec01d6236d3cd881a0bf0130ea25fe4234003e"),
@@ -81,7 +82,7 @@ var NetworkIDToContractAddresses = map[int]ContractAddresses{
 		Exchange:            common.HexToAddress("0x30589010550762d2f0d06f650d8e8b6ade6dbf4b"),
 		Coordinator:         common.HexToAddress("0x2ba02e03ee0029311e0f43715307870a3e701b53"),
 		CoordinatorRegistry: common.HexToAddress("0x09fb99968c016a3ff537bf58fb3d9fe55a7975d5"),
-		DevUtils:            common.HexToAddress("0x1e3616bc5144362f95d72de41874395567697e93"),
+		DevUtils:            common.HexToAddress("0xb1863ac46ae23ec55d6eeb8ecc8815655ee638a8"),
 		WETH9:               common.HexToAddress("0xd0a1e359811322d97991e03f863a0c30c2cf029c"),
 		ZRXToken:            common.HexToAddress("0x2002d3812f58e35f0ea1ffbf80a75a38c32175fa"),
 	},
