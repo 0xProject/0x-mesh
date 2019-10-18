75bd78cbaa31a7b279436ba1d87695e4e54421b2
Alex Browne <stephenalexbrowne@gmail.com>
2019-10-18 16:22:02 -0700

    Add test for SlowCounter.Reset

--- zeroex/orderwatch/slowcounter/slow_counter_test.go
+++ zeroex/orderwatch/slowcounter/slow_counter_test.go
@@ -98,7 +98,7 @@ func TestSlowCounterWithDelay(t *testing.T) {
 	counter, err := New(config, big.NewInt(0))
 	require.NoError(t, err)
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < config.MinTicksBeforeIncr+1; i++ {
 		wasIncremented := counter.Tick()
 		assert.False(t, wasIncremented, "counter should not be incremented before min delay")
 	}
@@ -113,7 +113,7 @@ func TestSlowCounterWithDelay(t *testing.T) {
 		assert.Equal(t, expectedCount, actualCount, "wrong count after counter was incremented")
 	}
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < config.MinTicksBeforeIncr+1; i++ {
 		wasIncremented := counter.Tick()
 		assert.False(t, wasIncremented, "counter should not be incremented before min delay")
 	}
@@ -126,5 +126,44 @@ func TestSlowCounterWithDelay(t *testing.T) {
 		expectedCount := big.NewInt(40)
 		actualCount := counter.Count()
 		assert.Equal(t, expectedCount, actualCount, "wrong count after counter was incremented")
+	}
+}
+
+func TestSlowCounterReset(t *testing.T) {
+	t.Parallel()
+
+	config := Config{
+		StartingOffset:     big.NewInt(10),
+		Rate:               2,
+		MinDelayBeforeIncr: 0,
+		MinTicksBeforeIncr: 3,
+		MaxCount:           big.NewInt(1000),
+	}
+	counter, err := New(config, big.NewInt(20))
+	require.NoError(t, err)
+
+	// Tick enough times to cause the counter to increment 5 times.
+	for i := 0; i < config.MinTicksBeforeIncr*5; i++ {
+		counter.Tick()
+	}
+
+	// Reset the counter and check that the count was correctly reset.
+	counter.Reset(big.NewInt(30))
+	{
+		expectedCount := big.NewInt(30)
+		actualCount := counter.Count()
+		assert.Equal(t, expectedCount, actualCount, "wrong count after counter was reset")
+	}
+
+	// Tick enough times to cause the counter to increment once.
+	for i := 0; i < config.MinTicksBeforeIncr; i++ {
+		counter.Tick()
+	}
+
+	// Check the counter was incremented once from the new value after reset.
+	{
+		expectedCount := big.NewInt(50)
+		actualCount := counter.Count()
+		assert.Equal(t, expectedCount, actualCount, "wrong count after counter was reset and then incremented")
 	}
 }
