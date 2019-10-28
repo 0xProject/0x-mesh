76325b974420630e2a62bec572b129d2bfe095f3
Alex Browne <stephenalexbrowne@gmail.com>
2019-10-28 13:50:33 -0700

    Log hash for orders added via RPC or browser callback

--- cmd/mesh/rpc_handler.go
+++ cmd/mesh/rpc_handler.go
@@ -72,7 +72,10 @@ func (handler *rpcHandler) GetOrders(page, perPage int, snapshotID string) (*rpc
 
 // AddOrders is called when an RPC client calls AddOrders.
 func (handler *rpcHandler) AddOrders(signedOrdersRaw []*json.RawMessage, opts rpc.AddOrdersOpts) (*ordervalidator.ValidationResults, error) {
-	log.WithField("count", len(signedOrdersRaw)).Debug("received AddOrders request via RPC")
+	log.WithFields(log.Fields{
+		"count":  len(signedOrdersRaw),
+		"pinned": opts.Pinned,
+	}).Info("received AddOrders request via RPC")
 	validationResults, err := handler.app.AddOrders(signedOrdersRaw, opts.Pinned)
 	if err != nil {
 		// We don't want to leak internal error details to the RPC client.
