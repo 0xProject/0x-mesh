22171aa2750fd3f15b8ef1fe7168c8fe3e803802
Alex Towle <jalextowle@gmail.com>
2019-10-27 23:06:29 -0700

    Added a serial test and refactored the test logic further

--- core/message_handler_test.go
+++ core/message_handler_test.go
@@ -78,45 +78,98 @@ func TestMessageSharingIsolated(t *testing.T) {
 		}
 
 		// Insert the orders into the database
-		for _, order := range orders {
-			err := selector.db.Orders.Insert(order)
-			require.NoError(t, err)
-		}
+		insertOrders(t, selector, orders)
 
-		notRemovedFilter := selector.db.Orders.IsRemovedIndex.ValueFilter([]byte{0})
-		ordersSnapshot, err := selector.db.Orders.GetSnapshot()
-		expectedOrdersLength := min(testCase.max, len(orders))
-		expectedOrders := make([][]byte, expectedOrdersLength)
-
-		// Get all of the orders in the database.
-		var orderList []*meshdb.Order
-		err = ordersSnapshot.NewQuery(notRemovedFilter).Offset(0).Max(len(orders)).Run(&orderList)
-
-		// Update `nextOffset` to zero if it is larger than the number of orders that are stored
-		if testCase.nextOffset > len(orders) {
-			testCase.nextOffset = 0
-		}
-
-		// Calculate the orders that we expect to be shared
-		for i := 0; i < expectedOrdersLength; i++ {
-			encoding, err := encodeOrder(orderList[(testCase.nextOffset+i)%len(orders)].SignedOrder)
-			require.NoError(t, err)
-
-			expectedOrders[i] = encoding
-		}
-
-		// Get the actual list of orders that are shared
-		actualOrders, err := selector.GetMessagesToShare(testCase.max)
-		require.NoError(t, err)
-
-		// Ensure that the result from `GetMessagesToShare` matches the expected result.
-		assert.Equal(t, expectedOrders, actualOrders)
+		// Ensure that the correct orders are shared
+		verifyRoundRobinSharing(t, selector, testCase.nextOffset, testCase.max)
 
 		// Delete the orders from the database
-		for _, order := range orders {
-			err := selector.db.Orders.Delete(order.ID())
-			require.NoError(t, err)
-		}
+		deleteOrders(t, selector, orders)
+	}
+}
+
+func TestMessagesSharedSerial(t *testing.T) {
+	var allOrders []*meshdb.Order
+	meshDB, err := meshdb.New("/tmp/leveldb_testing/" + uuid.New().String())
+	defer meshDB.Close()
+	require.NoError(t, err)
+
+	selector := &OrderSelector{
+		nextOffset: 0,
+		db:         meshDB,
+	}
+
+	// Add five orders to the database
+	orders := randomOrders(t, 5)
+	allOrders = orders
+	insertOrders(t, selector, orders)
+	verifyRoundRobinSharing(t, selector, 0, 3)
+
+	// Add seven more orders to the database
+	orders = randomOrders(t, 7)
+	allOrders = append(allOrders, orders...)
+	insertOrders(t, selector, orders)
+	verifyRoundRobinSharing(t, selector, 3, 10)
+
+	// Delete three orders from the database
+	deleteOrders(t, selector, allOrders[0:3:3])
+	verifyRoundRobinSharing(t, selector, selector.nextOffset, 5)
+
+	// Add 12 more orders to the database
+	orders = randomOrders(t, 12)
+	allOrders = append(allOrders, orders...)
+	insertOrders(t, selector, orders)
+	verifyRoundRobinSharing(t, selector, selector.nextOffset, 7)
+}
+
+// Verify that the correct messages are shared by `GetMessagesToShare` given `orders`, a `nextOffset`, and `max`
+func verifyRoundRobinSharing(t *testing.T, selector *OrderSelector, nextOffset int, max int) {
+	notRemovedFilter := selector.db.Orders.IsRemovedIndex.ValueFilter([]byte{0})
+	ordersSnapshot, err := selector.db.Orders.GetSnapshot()
+
+	// Get the number of orders in the database
+	count, err := ordersSnapshot.NewQuery(notRemovedFilter).Count()
+	require.NoError(t, err)
+
+	expectedOrdersLength := min(max, count)
+	expectedOrders := make([][]byte, expectedOrdersLength)
+
+	// Get all of the orders in the database.
+	var orderList []*meshdb.Order
+	err = ordersSnapshot.NewQuery(notRemovedFilter).Offset(0).Max(count).Run(&orderList)
+
+	// Update `nextOffset` to zero if it is larger than the number of orders that are stored
+	if nextOffset > count {
+		nextOffset = 0
+	}
+
+	// Calculate the orders that we expect to be shared
+	for i := 0; i < expectedOrdersLength; i++ {
+		encoding, err := encodeOrder(orderList[(nextOffset+i)%count].SignedOrder)
+		require.NoError(t, err)
+
+		expectedOrders[i] = encoding
+	}
+
+	// Get the actual list of orders that are shared
+	actualOrders, err := selector.GetMessagesToShare(max)
+	require.NoError(t, err)
+
+	// Ensure that the result from `GetMessagesToShare` matches the expected result.
+	assert.Equal(t, expectedOrders, actualOrders)
+}
+
+func deleteOrders(t *testing.T, selector *OrderSelector, orders []*meshdb.Order) {
+	for _, order := range orders {
+		err := selector.db.Orders.Delete(order.ID())
+		require.NoError(t, err)
+	}
+}
+
+func insertOrders(t *testing.T, selector *OrderSelector, orders []*meshdb.Order) {
+	for _, order := range orders {
+		err := selector.db.Orders.Insert(order)
+		require.NoError(t, err)
 	}
 }
 
