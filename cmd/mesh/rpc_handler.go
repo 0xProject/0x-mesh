fd6c7e66340e3f2a00450936741d0bfc2ee62e23
Alex Browne <stephenalexbrowne@gmail.com>
2019-11-21 13:29:02 -0800

    Revert "Always unsubscribe from order events in SetupOrderStream"

--- cmd/mesh/rpc_handler.go
+++ cmd/mesh/rpc_handler.go
@@ -22,10 +22,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// orderEventsBufferSize is the buffer size for the orderEvents channel. If
-// the buffer is full, any additional events won't be processed.
-const orderEventsBufferSize = 8000
-
 type rpcHandler struct {
 	app *core.App
 }
@@ -213,9 +209,8 @@ func SetupOrderStream(ctx context.Context, app *core.App) (*ethrpc.Subscription,
 	rpcSub := notifier.CreateSubscription()
 
 	go func() {
-		orderEventsChan := make(chan []*zeroex.OrderEvent, orderEventsBufferSize)
+		orderEventsChan := make(chan []*zeroex.OrderEvent)
 		orderWatcherSub := app.SubscribeToOrderEvents(orderEventsChan)
-		defer orderWatcherSub.Unsubscribe()
 
 		for {
 			select {
@@ -242,6 +237,7 @@ func SetupOrderStream(ctx context.Context, app *core.App) (*ethrpc.Subscription,
 					// error.
 					if _, ok := err.(*net.OpError); ok {
 						logEntry.Trace(message)
+						orderWatcherSub.Unsubscribe()
 						return
 					}
 					if strings.Contains(err.Error(), "write: broken pipe") {
@@ -253,11 +249,13 @@ func SetupOrderStream(ctx context.Context, app *core.App) (*ethrpc.Subscription,
 			case err := <-rpcSub.Err():
 				if err != nil {
 					log.WithField("err", err).Error("rpcSub returned an error")
+					orderWatcherSub.Unsubscribe()
 				} else {
 					log.Debug("rpcSub was closed without error")
 				}
 				return
 			case <-notifier.Closed():
+				orderWatcherSub.Unsubscribe()
 				return
 			}
 		}
