482383322d19edcbf9a78239576b7fc6473385d0
fabioberger <me@fabioberger.com>
2019-12-01 21:22:22 +0100

    Fix comments

--- constants/constants.go
+++ constants/constants.go
@@ -28,16 +28,16 @@ var (
 	// GanacheAccount0 is the first account exposed on the Ganache test Ethereum node
 	GanacheAccount0           = common.HexToAddress("0x5409ed021d9299bf6814279a6a1411a7e866a631")
 	ganacheAccount0PrivateKey = common.Hex2Bytes("f2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d")
-	// GanacheAccount1 is the first account exposed on the Ganache test Ethereum node
+	// GanacheAccount1 is the second account exposed on the Ganache test Ethereum node
 	GanacheAccount1           = common.HexToAddress("0x6ecbe1db9ef729cbe972c83fb886247691fb6beb")
 	ganacheAccount1PrivateKey = common.Hex2Bytes("5d862464fe9303452126c8bc94274b8c5f9874cbd219789b3eb2128075a76f72")
-	// GanacheAccount2 is the first account exposed on the Ganache test Ethereum node
+	// GanacheAccount2 is the third account exposed on the Ganache test Ethereum node
 	GanacheAccount2           = common.HexToAddress("0xe36ea790bc9d7ab70c55260c66d52b1eca985f84")
 	ganacheAccount2PrivateKey = common.Hex2Bytes("df02719c4df8b9b8ac7f551fcb5d9ef48fa27eef7a66453879f4d8fdc6e78fb1")
-	// GanacheAccount3 is the first account exposed on the Ganache test Ethereum node
+	// GanacheAccount3 is the fourth account exposed on the Ganache test Ethereum node
 	GanacheAccount3           = common.HexToAddress("0xe834ec434daba538cd1b9fe1582052b880bd7e63")
 	ganacheAccount3PrivateKey = common.Hex2Bytes("ff12e391b79415e941a94de3bf3a9aee577aed0731e297d5cfa0b8a1e02fa1d0")
-	// GanacheAccount4 is the first account exposed on the Ganache test Ethereum node
+	// GanacheAccount4 is the fifth account exposed on the Ganache test Ethereum node
 	GanacheAccount4           = common.HexToAddress("0x78dc5d2d739606d31509c31d654056a45185ecb6")
 	ganacheAccount4PrivateKey = common.Hex2Bytes("752dd9cf65e68cfaba7d60225cbdbc1f4729dd5e5507def72815ed0d8abc6249")
 )
