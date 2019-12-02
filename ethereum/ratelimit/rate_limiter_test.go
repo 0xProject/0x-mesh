ebf74521c92022e6a762e8bb8847422eb0820206
fabioberger <me@fabioberger.com>
2019-12-02 12:19:25 +0100

    Increase max expected delay to match min expected delay

--- ethereum/ratelimit/rate_limiter_test.go
+++ ethereum/ratelimit/rate_limiter_test.go
@@ -25,7 +25,7 @@ var (
 	defaultCheckpointInterval             = 1 * time.Minute
 	expectedMaxElapsedTimeForFirstRequest = 1 * time.Millisecond
 	expectedDeltaMinExpectedDelay         = 20 * time.Millisecond
-	expectedDeltaMaxExpectedDelay         = 15 * time.Millisecond
+	expectedDeltaMaxExpectedDelay         = 20 * time.Millisecond
 )
 
 // Scenario1: Mesh starts X seconds after UTC midnight (start of next UTC day) and
