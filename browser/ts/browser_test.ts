0604b2cddef0c096a3798183645567f68ef9eb83
Alex Towle <jalextowle@gmail.com>
2020-01-16 15:08:53 -0800

    Set up the typescript scaffolding for the browser conversion tests

---
+++ browser/ts/browser_test.ts
@@ -0,0 +1,12 @@
+import {
+    configToWrapperConfig,
+    orderEventsHandlerToWrapperOrderEventsHandler,
+    signedOrderToWrapperSignedOrder,
+    wrapperAcceptedOrderInfoToAcceptedOrderInfo,
+    wrapperContractEventsToContractEvents,
+    wrapperOrderEventToOrderEvent,
+    wrapperRejectedOrderInfoToRejectedOrderInfo,
+    wrapperSignedOrderToSignedOrder,
+    wrapperValidationResultsToValidationResults,
+} from './encoding';
+import './wasm_exec';
