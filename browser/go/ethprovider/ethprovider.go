5befd4d54674a5e525aee4196bec52aabadc729b
Alex Browne <stephenalexbrowne@gmail.com>
2020-01-21 15:43:01 -0800

    Add stubs for a browser implementation of ethclient.RPCClient

---
+++ browser/go/ethprovider/ethprovider.go
@@ -0,0 +1,42 @@
+// +build js,wasm
+
+package ethprovider
+
+import (
+	"context"
+	"errors"
+	"syscall/js"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+var _ ethclient.RPCClient = &RPCClient{}
+
+type RPCClient struct {
+	// provider is the underlying Web3 provider which will be used for sending
+	// requests.
+	provider js.Value
+}
+
+func NewRPCClient(provider js.Value) *RPCClient {
+	return &RPCClient{
+		provider: provider,
+	}
+}
+
+func (c *RPCClient) CallContext(ctx context.Context, result interface{}, method string, args ...interface{}) error {
+	return errors.New("CallContext not yet implemented")
+}
+
+func (c *RPCClient) BatchCallContext(ctx context.Context, b []rpc.BatchElem) error {
+	return errors.New("BatchCallContext not yet implemented")
+}
+
+func (c *RPCClient) EthSubscribe(ctx context.Context, channel interface{}, args ...interface{}) (*rpc.ClientSubscription, error) {
+	return nil, errors.New("EthSubscribe not yet implemented")
+}
+
+func (c *RPCClient) Close() {
+	// no-op for now.
+}
