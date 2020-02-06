8ae9be53cdc6d8e40418665c009e00cd92a2859e
Alex Towle <jalextowle@gmail.com>
2020-02-06 10:39:56 -0800

    Started creating the monorepo

---
+++ typescript/packages/mesh-browser/src/index.ts
@@ -0,0 +1,24 @@
+import 'wasm_exec.ts';
+
+import { wasmBuffer } from './generated/wasm_buffer';
+
+export * from '@0x/mesh-browser-lib';
+
+// Start compiling the WebAssembly as soon as the script is loaded. This lets
+// us initialize as quickly as possible.
+const go = new Go();
+WebAssembly.instantiate(wasmBuffer, go.importObject)
+    .then(module => {
+        go.run(module.instance);
+    })
+    .catch(err => {
+        // tslint:disable-next-line no-console
+        console.error('Could not load Wasm');
+        // tslint:disable-next-line no-console
+        console.error(err);
+        // If the Wasm bytecode didn't compile, Mesh won't work. We have no
+        // choice but to throw an error.
+        setImmediate(() => {
+            throw err;
+        });
+    });
