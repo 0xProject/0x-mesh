61a42fe15c2cfcde97f18629c7e79185ec0b5ff2
fabioberger <me@fabioberger.com>
2020-02-24 13:33:37 +0100

    Remove unnecessary `go  ` statement

--- cmd/mesh-bridge/main.go
+++ cmd/mesh-bridge/main.go
@@ -64,7 +64,7 @@ func main() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		go pipeOrders(firstClient, secondClient, firstWSRPCAddressLabel, secondWSRPCAddressLabel)
+		pipeOrders(firstClient, secondClient, firstWSRPCAddressLabel, secondWSRPCAddressLabel)
 	}()
 
 	wg.Wait()
