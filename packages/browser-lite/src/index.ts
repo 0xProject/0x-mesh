6aea87f746b946ed8b6e4276c6a8e96463301b66
Alex Towle <jalextowle@gmail.com>
2020-03-02 17:09:07 -0800

    Promisified the `@0x/mesh-browser-lite` API (#740)
    * made the browser-lite api async
    
    * Switch to using await syntax

--- packages/browser-lite/src/index.ts
+++ packages/browser-lite/src/index.ts
@@ -4,29 +4,16 @@ export * from './mesh';
  * Loads the Wasm module that is provided by fetching a url.
  * @param url The URL to query for the Wasm binary.
  */
-export function loadMeshStreamingWithURL(url: string): void {
-    loadMeshStreaming(fetch(url));
+export async function loadMeshStreamingWithURLAsync(url: string): Promise<void> {
+    return loadMeshStreamingAsync(fetch(url));
 }
 
 /**
  * Loads the Wasm module that is provided by a response.
  * @param response The Wasm response that supplies the Wasm binary.
  */
-export function loadMeshStreaming(response: Response | Promise<Response>): void {
+export async function loadMeshStreamingAsync(response: Response | Promise<Response>): Promise<void> {
     const go = new Go();
-    WebAssembly.instantiateStreaming(response, go.importObject)
-        .then(module => {
-            go.run(module.instance);
-        })
-        .catch(err => {
-            // tslint:disable-next-line no-console
-            console.error('Could not load Wasm');
-            // tslint:disable-next-line no-console
-            console.error(err);
-            // If the Wasm bytecode didn't compile, Mesh won't work. We have no
-            // choice but to throw an error.
-            setImmediate(() => {
-                throw err;
-            });
-        });
+    const module = await WebAssembly.instantiateStreaming(response, go.importObject);
+    return go.run(module.instance);
 }
