e38cc2c1e272b0fa39afbbddf55dedbef4b05fb6
fabioberger <me@fabioberger.com>
2020-04-02 11:32:24 +0200

    Fix bug causing orders expiring _at exactly block timestamp_ from not being pruned until the next block is mined

--- expirationwatch/expiration_watcher_test.go
+++ expirationwatch/expiration_watcher_test.go
@@ -29,6 +29,27 @@ func TestPrunesExpiredItems(t *testing.T) {
 	assert.Equal(t, expiryEntryTwo, pruned[1])
 }
 
+func TestPrunesItemsExpiringAtCurrentTime(t *testing.T) {
+	watcher := New()
+
+	current := time.Now().Truncate(time.Second)
+	expiryEntryOne := ExpiredItem{
+		ExpirationTimestamp: current,
+		ID:                  "0x8e209dda7e515025d0c34aa61a0d1156a631248a4318576a2ce0fb408d97385e",
+	}
+	watcher.Add(expiryEntryOne.ExpirationTimestamp, expiryEntryOne.ID)
+
+	expiryEntryTwo := ExpiredItem{
+		ExpirationTimestamp: current.Add(5 * time.Second),
+		ID:                  "0x12ab7edd34515025d0c34aa61a0d1156a631248a4318576a2ce0fb408d3bee521",
+	}
+	watcher.Add(expiryEntryTwo.ExpirationTimestamp, expiryEntryTwo.ID)
+
+	pruned := watcher.Prune(current)
+	assert.Len(t, pruned, 1, "one expired item should get pruned")
+	assert.Equal(t, expiryEntryOne, pruned[0])
+}
+
 func TestPrunesTwoExpiredItemsWithSameExpiration(t *testing.T) {
 	watcher := New()
 
