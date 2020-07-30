e9dde922f5dc3b9f346cb154d21588b761aa0245
Alex Towle <jalextowle@gmail.com>
2020-07-30 14:53:23 -0500

    Added another test case

--- core/ordersync/ordersync_test.go
+++ core/ordersync/ordersync_test.go
@@ -30,7 +30,6 @@ func TestCalculateDelayWithJitters(t *testing.T) {
 }
 
 func TestHandleRawRequest(t *testing.T) {
-	// FIXME - How to get this to not set up any directories?
 	n, err := p2p.New(
 		context.Background(),
 		p2p.Config{
@@ -48,24 +47,52 @@ func TestHandleRawRequest(t *testing.T) {
 	}
 	s := New(context.Background(), n, []Subprotocol{subp0, subp1})
 
+	// Simulate encoding the request, sending it over the wire, and decoding
+	// it by simply encoding and then decoding the raw request.
+	rawReq := &rawRequest{
+		Type:         TypeRequest,
+		Subprotocols: []string{subp0.Name(), subp1.Name()},
+	}
+	encodedReq, err := json.Marshal(rawReq)
+	require.NoError(t, err)
+	decodedReq := &rawRequest{}
+	err = json.Unmarshal(encodedReq, decodedReq)
+	require.NoError(t, err)
 	// This request has multiple subprotocols included and nil metadata. This
 	// has the same structure as requests that would have been sent by older
 	// versions of Mesh, and allows us to test that newer Mesh nodes provide
 	// backwards compatability as ordersync providers.
-	var metadata simpleOrderSyncSubprotocolRequestMetadata
-	encodedMetadata, err := json.Marshal(metadata)
-	require.NoError(t, err)
-	res := s.handleRawRequest(&rawRequest{
-		Type:         TypeRequest,
-		Subprotocols: []string{subp0.Name(), subp1.Name()},
-		Metadata:     encodedMetadata,
-	}, n.ID())
+	res := s.handleRawRequest(decodedReq, n.ID())
 	require.NotNil(t, res)
 	assert.True(t, res.Complete)
 	assert.Equal(t, 1, len(res.Orders))
 	assert.Equal(t, res.Subprotocol, subp0.Name())
+	// Ensure that the correct metadata was returned. We expect the
+	decodedMetadata := simpleOrderSyncSubprotocolRequestMetadataV0{}
+	err = json.Unmarshal(decodedReq.Metadata, &decodedMetadata)
+	require.NoError(t, err)
+	encodedMetadata, err := json.Marshal(decodedMetadata)
+	require.NoError(t, err)
+	assert.Equal(t, res.Metadata, json.RawMessage(encodedMetadata))
 
-	// FIXME - Add more test cases for current nodes
+	// Test handling a request from a node that is using the new first request
+	// encoding scheme.
+	rawReq, err = s.createFirstRequestForAllSubprotocols()
+	require.NoError(t, err)
+	encodedReq, err = json.Marshal(rawReq)
+	require.NoError(t, err)
+	decodedReq = &rawRequest{}
+	err = json.Unmarshal(encodedReq, decodedReq)
+	require.NoError(t, err)
+	res = s.handleRawRequest(decodedReq, n.ID())
+	require.NotNil(t, res)
+	assert.True(t, res.Complete)
+	assert.Equal(t, 1, len(res.Orders))
+	assert.Equal(t, res.Subprotocol, subp0.Name())
+	// Ensure that the correct metadata was returned
+	encodedMetadata, err = subp0.GenerateFirstRequestMetadata()
+	require.NoError(t, err)
+	assert.Equal(t, res.Metadata, json.RawMessage(encodedMetadata))
 }
 
 var _ p2p.MessageHandler = &simpleMessageHandler{}
@@ -81,11 +108,12 @@ var _ Subprotocol = &simpleOrderSyncSubprotocolV0{}
 type simpleOrderSyncSubprotocolV0 struct {
 	myPeerID peer.ID
 }
-type simpleOrderSyncSubprotocolRequestMetadata struct {
+
+type simpleOrderSyncSubprotocolRequestMetadataV0 struct {
 	SomeValue interface{} `json:"someValue"`
 }
 
-type simpleOrderSyncSubprotocolResponseMetadata struct {
+type simpleOrderSyncSubprotocolResponseMetadataV0 struct {
 	SomeValue interface{} `json:"someValue"`
 }
 
@@ -94,10 +122,7 @@ func (s *simpleOrderSyncSubprotocolV0) Name() string {
 }
 
 func (s *simpleOrderSyncSubprotocolV0) ParseRequestMetadata(metadata json.RawMessage) (interface{}, error) {
-	if metadata == nil {
-		return nil, nil
-	}
-	var parsed simpleOrderSyncSubprotocolRequestMetadata
+	var parsed simpleOrderSyncSubprotocolRequestMetadataV0
 	if err := json.Unmarshal(metadata, &parsed); err != nil {
 		return nil, err
 	}
@@ -105,10 +130,7 @@ func (s *simpleOrderSyncSubprotocolV0) ParseRequestMetadata(metadata json.RawMes
 }
 
 func (s *simpleOrderSyncSubprotocolV0) ParseResponseMetadata(metadata json.RawMessage) (interface{}, error) {
-	if metadata == nil {
-		return nil, nil
-	}
-	var parsed simpleOrderSyncSubprotocolRequestMetadata
+	var parsed simpleOrderSyncSubprotocolResponseMetadataV0
 	if err := json.Unmarshal(metadata, &parsed); err != nil {
 		return nil, err
 	}
@@ -142,19 +164,21 @@ func (s *simpleOrderSyncSubprotocolV0) HandleOrderSyncRequest(ctx context.Contex
 		ProviderID: s.myPeerID,
 		Orders:     []*zeroex.SignedOrder{signedOrder},
 		Complete:   true,
-		Metadata:   nil,
+		Metadata:   req.Metadata,
 	}, nil
 }
 
 func (s *simpleOrderSyncSubprotocolV0) HandleOrderSyncResponse(ctx context.Context, res *Response) (*Request, int, error) {
 	return &Request{
 		RequesterID: s.myPeerID,
-		Metadata:    nil,
+		Metadata:    res.Metadata,
 	}, len(res.Orders), nil
 }
 
 func (s *simpleOrderSyncSubprotocolV0) GenerateFirstRequestMetadata() (json.RawMessage, error) {
-	return json.Marshal(simpleOrderSyncSubprotocolRequestMetadata{})
+	return json.Marshal(simpleOrderSyncSubprotocolRequestMetadataV0{
+		SomeValue: 0,
+	})
 }
 
 var _ Subprotocol = &simpleOrderSyncSubprotocolV1{}
@@ -164,16 +188,32 @@ type simpleOrderSyncSubprotocolV1 struct {
 	hostSubp *simpleOrderSyncSubprotocolV0
 }
 
+type simpleOrderSyncSubprotocolRequestMetadataV1 struct {
+	AnotherValue interface{} `json:"anotherValue"`
+}
+
+type simpleOrderSyncSubprotocolResponseMetadataV1 struct {
+	AnotherValue interface{} `json:"anotherValue"`
+}
+
 func (s *simpleOrderSyncSubprotocolV1) Name() string {
 	return "/simple-order-sync-subprotocol/v1"
 }
 
-func (s *simpleOrderSyncSubprotocolV1) ParseRequestMetadata(encodedMetadata json.RawMessage) (interface{}, error) {
-	return s.ParseRequestMetadata(encodedMetadata)
+func (s *simpleOrderSyncSubprotocolV1) ParseRequestMetadata(metadata json.RawMessage) (interface{}, error) {
+	var parsed simpleOrderSyncSubprotocolRequestMetadataV1
+	if err := json.Unmarshal(metadata, &parsed); err != nil {
+		return nil, err
+	}
+	return &parsed, nil
 }
 
-func (s *simpleOrderSyncSubprotocolV1) ParseResponseMetadata(encodedMetadata json.RawMessage) (interface{}, error) {
-	return s.ParseResponseMetadata(encodedMetadata)
+func (s *simpleOrderSyncSubprotocolV1) ParseResponseMetadata(metadata json.RawMessage) (interface{}, error) {
+	var parsed simpleOrderSyncSubprotocolResponseMetadataV1
+	if err := json.Unmarshal(metadata, &parsed); err != nil {
+		return nil, err
+	}
+	return &parsed, nil
 }
 
 func (s *simpleOrderSyncSubprotocolV1) HandleOrderSyncRequest(ctx context.Context, req *Request) (*Response, error) {
@@ -185,5 +225,7 @@ func (s *simpleOrderSyncSubprotocolV1) HandleOrderSyncResponse(ctx context.Conte
 }
 
 func (s *simpleOrderSyncSubprotocolV1) GenerateFirstRequestMetadata() (json.RawMessage, error) {
-	return s.hostSubp.GenerateFirstRequestMetadata()
+	return json.Marshal(simpleOrderSyncSubprotocolRequestMetadataV1{
+		AnotherValue: 1,
+	})
 }
