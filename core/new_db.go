80fffadc64337a3d07cd32b2458f1eddeeac4cbc
opaolini <oskar@paolini.pl>
2020-12-03 16:34:58 +0100

    use the same defaults for newDB

--- core/new_db.go
+++ core/new_db.go
@@ -10,15 +10,15 @@ import (
 )
 
 func newDB(ctx context.Context, config Config) (*db.DB, error) {
-	meshDatabasePath := filepath.Join(config.DataDir, "sqlite-db", "db.sqlite?_journal=WAL")
-	peerStoreDatabasePath := filepath.Join(config.DataDir, "sqlite-db", "peerstore.sqlite?_journal=WAL")
-	dhtDatabasePath := filepath.Join(config.DataDir, "sqlite-db", "dht.sqlite?_journal=WAL")
+	meshDatabasePath := filepath.Join(config.DataDir, "db", "db.sqlite?_journal=WAL")
+	peerStoreDatabasePath := filepath.Join(config.DataDir, "db", "peerstore.sqlite?_journal=WAL")
+	dhtDatabasePath := filepath.Join(config.DataDir, "db", "dht.sqlite?_journal=WAL")
+
 	return db.New(ctx, &db.Options{
 		DriverName:              "sqlite3",
 		DataSourceName:          meshDatabasePath,
 		DataSourcePeerStoreName: peerStoreDatabasePath,
 		DataSourceDHTName:       dhtDatabasePath,
-		DataDir:                 config.DataDir,
 		MaxOrders:               config.MaxOrdersInStorage,
 	})
 }
