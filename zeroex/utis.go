a38a30e44acc96335ee20b8c1bb8efb10aa8cc55
Remco Bloemen <remco@0x.org>
2021-01-13 19:05:55 -0800

    Add Bytes32 type

--- zeroex/utis.go
+++ zeroex/utis.go
@@ -2,6 +2,9 @@ package zeroex
 
 import (
 	"golang.org/x/crypto/sha3"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 // keccak256 calculates and returns the Keccak256 hash of the input data.
@@ -11,4 +14,36 @@ func keccak256(data ...[]byte) []byte {
 		_, _ = d.Write(b)
 	}
 	return d.Sum(nil)
+}
+
+// Bytes32 represents the Solidity `bytes32` type.
+// It is mostly copied from go-ethereum's Hash type.
+// See <https://github.com/ethereum/go-ethereum/blob/053ed9cc847647a9b3ef707d0efe7104c4ab2a4c/common/types.go#L47>
+type Bytes32 [32]byte
+
+// BytesToBytes32 converts []byte to Bytes32
+func BytesToBytes32(b []byte) Bytes32 {
+	var result Bytes32
+	result.SetBytes(b)
+	return result
+}
+
+// HexToBytes32 creates a Bytes32 from a hex string.
+func HexToBytes32(s string) Bytes32 {
+	return BytesToBytes32(common.FromHex(s))
+}
+
+// SetBytes sets the Bytes32 to the value of bytes.
+// If bytes is larger than 32, bytes will be cropped from the left.
+func (b *Bytes32) SetBytes(bytes []byte) {
+	if len(bytes) > 32 {
+		bytes = bytes[len(bytes)-32:]
+	}
+
+	copy(b[32-len(bytes):], bytes)
+}
+
+// Hex converts a Bytes32 to a hex string.
+func (b Bytes32) Hex() string {
+	return hexutil.Encode(b[:])
 }
