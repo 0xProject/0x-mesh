0b977044dcb0f9ae41517680a5cd169b579a7b96
Remco Bloemen <remco@0x.org>
2021-01-20 20:14:51 -0800

    Add Cancelled event

--- zeroex/orderwatch/decoder/event_decoder_v4.go
+++ zeroex/orderwatch/decoder/event_decoder_v4.go
@@ -1,10 +1,19 @@
 package decoder
 
 import (
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/common"
 )
 
 type ExchangeCancelEventV4 struct {
 	Maker     common.Address
 	OrderHash common.Hash
+}
+
+type ExchangePairCancelledLimitOrdersEventV4 struct {
+	Maker        common.Address
+	MakerToken   common.Address
+	TakerToken   common.Address
+	MinValidSalt *big.Int
 }
