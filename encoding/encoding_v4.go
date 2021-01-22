504a053e67886984c0229fac641d440162c0356a
Remco Bloemen <remco@0x.org>
2021-01-22 10:11:23 -0800

    Simplify

--- encoding/encoding_v4.go
+++ encoding/encoding_v4.go
@@ -2,32 +2,18 @@ package encoding
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"github.com/0xProject/0x-mesh/zeroex"
 )
 
-type orderMessageV4 struct {
-	MessageType string                `json:"messageType"`
-	Order       *zeroex.SignedOrderV4 `json:"order"`
-	Topics      []string              `json:"topics"`
-}
-
 func OrderToRawMessageV4(topic string, order *zeroex.SignedOrderV4) ([]byte, error) {
-	return json.Marshal(orderMessageV4{
-		MessageType: "order",
-		Order:       order,
-		Topics:      []string{topic},
-	})
+	return json.Marshal(order)
 }
 
 func RawMessageToOrderV4(data []byte) (*zeroex.SignedOrderV4, error) {
-	var orderMessage orderMessage
-	if err := json.Unmarshal(data, &orderMessage); err != nil {
+	var order zeroex.SignedOrderV4
+	if err := json.Unmarshal(data, &order); err != nil {
 		return nil, err
 	}
-	if orderMessage.MessageType != "order" {
-		return nil, fmt.Errorf("unexpected message type: %q", orderMessage.MessageType)
-	}
-	return orderMessage.Order, nil
+	return &order, nil
 }
