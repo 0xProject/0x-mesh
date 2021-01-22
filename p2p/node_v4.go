e0a074719aff330a1aa311407cbf1c805f7cb32b
Remco Bloemen <remco@0x.org>
2021-01-22 09:53:19 -0800

    Simplify

--- p2p/node_v4.go
+++ p2p/node_v4.go
@@ -21,67 +21,15 @@ func (n *Node) SendV4(data []byte) error {
 	return firstErr
 }
 
-func (n *Node) receiveV4(ctx context.Context) (*Message, error) {
+func (n *Node) startMessageHandlerV4(ctx context.Context) error {
+	// Subscribe to topic if we haven't already
 	if n.subV4 == nil {
 		var err error
 		n.subV4, err = n.pubsub.Subscribe(gossipSubOrdersV4Topic)
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
-	msg, err := n.subV4.Next(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return &Message{From: msg.GetFrom(), Data: msg.Data}, nil
-}
-
-// receiveBatch returns up to maxReceiveBatch messages which are received from
-// peers. There is no guarantee that the messages are unique.
-func (n *Node) receiveBatchV4(ctx context.Context) ([]*Message, error) {
-	messages := []*Message{}
-	for {
-		if len(messages) >= maxReceiveBatch {
-			return messages, nil
-		}
-		select {
-		// If the parent context was canceled, return.
-		case <-ctx.Done():
-			return messages, nil
-		default:
-		}
-		receiveCtx, receiveCancel := context.WithTimeout(n.ctx, receiveTimeout)
-		msg, err := n.receiveV4(receiveCtx)
-		receiveCancel()
-		if err != nil {
-			if err == context.Canceled || err == context.DeadlineExceeded {
-				return messages, nil
-			}
-			return nil, err
-		}
-		if msg.From == n.host.ID() {
-			continue
-		}
-		messages = append(messages, msg)
-	}
-}
-
-func (n *Node) receiveAndHandleMessagesV4(ctx context.Context) error {
-	// Receive up to maxReceiveBatch messages.
-	incoming, err := n.receiveBatchV4(ctx)
-	if err != nil {
-		return err
-	}
-	if len(incoming) == 0 {
-		return nil
-	}
-	if err := n.messageHandler.HandleMessagesV4(ctx, incoming); err != nil {
-		return fmt.Errorf("could not validate or store messages: %s", err.Error())
-	}
-	return nil
-}
-
-func (n *Node) startMessageHandlerV4(ctx context.Context) error {
 	for {
 		select {
 		case <-ctx.Done():
@@ -89,8 +37,42 @@ func (n *Node) startMessageHandlerV4(ctx context.Context) error {
 		default:
 		}
 
-		if err := n.receiveAndHandleMessagesV4(ctx); err != nil {
-			return err
+		// Receive a batch of messages so we can handle them in bulk
+		messages := []*Message{}
+		for {
+			// If the parent context was canceled, break.
+			select {
+			case <-ctx.Done():
+				break
+			default:
+			}
+
+			// Receive message
+			receiveCtx, receiveCancel := context.WithTimeout(n.ctx, receiveTimeout)
+			msg, err := n.subV4.Next(receiveCtx)
+			receiveCancel()
+			if err != nil {
+				if err == context.Canceled || err == context.DeadlineExceeded {
+					break
+				} else {
+					return err
+				}
+			}
+			// Skip self messages
+			if msg.GetFrom() == n.host.ID() {
+				continue
+			}
+
+			// Add to batch
+			messages = append(messages, &Message{From: msg.GetFrom(), Data: msg.Data})
+			if len(messages) >= maxReceiveBatch {
+				break
+			}
+		}
+
+		// Handle messages
+		if err := n.messageHandler.HandleMessagesV4(ctx, messages); err != nil {
+			return fmt.Errorf("could not validate or store V4 order messages: %s", err.Error())
 		}
 
 		// Check bandwidth usage non-deterministically
